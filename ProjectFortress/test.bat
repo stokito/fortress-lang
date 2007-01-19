da12de26353a0e0575b1f4f240e9d6b86a7f91e2
dc12360 <dc12360@localhost>
2007-01-19 16:34:59 +0000

    Run/test fixes for Windows

---
+++ ProjectFortress/test.bat
@@ -0,0 +1,18 @@
+REM ################################################################################
+REM #    Copyright 2007 Sun Microsystems, Inc.,
+REM #    4150 Network Circle, Santa Clara, California 95054, U.S.A.
+REM #    All rights reserved.
+REM #
+REM #    U.S. Government Rights - Commercial software.
+REM #    Government users are subject to the Sun Microsystems, Inc. standard
+REM #    license agreement and applicable provisions of the FAR and its supplements.
+REM #
+REM #    Use is subject to license terms.
+REM #
+REM #    This distribution may include materials developed by third parties.
+REM #
+REM #    Sun, Sun Microsystems, the Sun logo and Java are trademarks or registered
+REM #    trademarks of Sun Microsystems, Inc. in the U.S. and other countries.
+REM ################################################################################
+
+java -cp "build;third_party/junit/junit.jar;third_party/xtc/xtc.jar;third_party/FJ/concurrent.jar;third_party/dstm2/dstm2.jar;third_party/bcel/bcel-5.2.jar" junit.swingui.TestRunner com.sun.fortress.interpreter.drivers.SystemJUTests
