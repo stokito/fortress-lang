1a62fbc43c30b8664bf8fa8e5f1712c92ccb92c1
sr155864 <sr155864@localhost>
2007-02-01 15:00:21 +0000

    Trait headers can have the excludes, comprises, and where clauses in any order.

---
+++ ProjectFortress/src/com/sun/fortress/interpreter/parser/Comprises.java
@@ -0,0 +1,50 @@
+/*******************************************************************************
+    Copyright 2007 Sun Microsystems, Inc.,
+    4150 Network Circle, Santa Clara, California 95054, U.S.A.
+    All rights reserved.
+
+    U.S. Government Rights - Commercial software.
+    Government users are subject to the Sun Microsystems, Inc. standard
+    license agreement and applicable provisions of the FAR and its supplements.
+
+    Use is subject to license terms.
+
+    This distribution may include materials developed by third parties.
+
+    Sun, Sun Microsystems, the Sun logo and Java are trademarks or registered
+    trademarks of Sun Microsystems, Inc. in the U.S. and other countries.
+ ******************************************************************************/
+
+/*
+ * Fortress comprises clause.
+ * Fortress AST node local to the Rats! com.sun.fortress.interpreter.parser.
+ */
+package com.sun.fortress.interpreter.parser;
+
+import java.util.List;
+import com.sun.fortress.interpreter.nodes.Span;
+import com.sun.fortress.interpreter.nodes.Option;
+import com.sun.fortress.interpreter.nodes.Some;
+import com.sun.fortress.interpreter.nodes.None;
+import com.sun.fortress.interpreter.nodes.TypeRef;
+
+public class Comprises extends TraitClause {
+    private Option<List<TypeRef>> comprises = None.<List<TypeRef>>make();
+
+    public Comprises(Span span, List<TypeRef> comprises) {
+        super(span);
+        this.comprises = Some.<List<TypeRef>>make(comprises);
+    }
+
+    public Option<List<TypeRef>> getComprises() {
+        return comprises;
+    }
+
+    public String message() {
+        return "comprises";
+    }
+
+    public Span span() {
+        return span;
+    }
+}
