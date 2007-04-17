b0be8b927a3557d0ae11e9bb630d31d7bbd9124e
sr155864 <sr155864@localhost>
2007-04-17 03:37:32 +0000

    Comprises clauses with ellipses are allowed only in APIs.

--- ProjectFortress/src/com/sun/fortress/interpreter/parser/TraitHeader.java
+++ ProjectFortress/src/com/sun/fortress/interpreter/parser/TraitHeader.java
@@ -24,10 +24,8 @@ import java.util.List;
 
 import com.sun.fortress.interpreter.nodes.Id;
 import com.sun.fortress.interpreter.nodes.Modifier;
-import com.sun.fortress.interpreter.nodes.Option;
 import com.sun.fortress.interpreter.nodes.StaticParam;
 import com.sun.fortress.interpreter.nodes.TypeRef;
-import com.sun.fortress.interpreter.nodes.WhereClause;
 import com.sun.fortress.interpreter.useful.MagicNumbers;
 
 public class TraitHeader {
@@ -36,22 +34,14 @@ public class TraitHeader {
     private Id name;
     private List<StaticParam> staticParams;
     private List<TypeRef> extendsClause;
-    private List<TypeRef> excludesClause;
-    private Option<List<TypeRef>> comprisesClause;
-    private Option<List<WhereClause>> whereClause;
 
     public TraitHeader(List<Modifier> mods, Id name,
                        List<StaticParam> staticParams,
-                       List<TypeRef> extendsClause, List<TypeRef> excludesClause,
-                       Option<List<TypeRef>> comprisesClause,
-                       Option<List<WhereClause>> whereClause) {
+                       List<TypeRef> extendsClause) {
         this.mods = mods;
         this.name = name;
         this.staticParams = staticParams;
         this.extendsClause = extendsClause;
-        this.excludesClause = excludesClause;
-        this.comprisesClause = comprisesClause;
-        this.whereClause = whereClause;
     }
 
     public List<Modifier> getMods() {
@@ -70,26 +60,11 @@ public class TraitHeader {
         return extendsClause;
     }
 
-    public List<TypeRef> getExcludesClause() {
-        return excludesClause;
-    }
-
-    public Option<List<TypeRef>> getComprisesClause() {
-        return comprisesClause;
-    }
-
-    public Option<List<WhereClause>> getWhereClause() {
-        return whereClause;
-    }
-
     public int hashCode() {
         return MagicNumbers.hashList(mods, MagicNumbers.m)
             + name.hashCode() * MagicNumbers.n
             + MagicNumbers.hashList(staticParams, MagicNumbers.e)
-            + MagicNumbers.hashList(extendsClause, MagicNumbers.l)
-            + MagicNumbers.hashList(excludesClause, MagicNumbers.y)
-            + comprisesClause.hashCode() * MagicNumbers.a
-            + whereClause.hashCode() * MagicNumbers.t;
+            + MagicNumbers.hashList(extendsClause, MagicNumbers.l);
     }
 
     public boolean equals(Object o) {
@@ -98,10 +73,7 @@ public class TraitHeader {
             return mods.equals(th.getMods())
                 && name.equals(th.getName())
                 && staticParams.equals(th.getStaticParams())
-                && extendsClause.equals(th.getExtendsClause())
-                && excludesClause.equals(th.getExcludesClause())
-                && comprisesClause.equals(th.getComprisesClause())
-                && whereClause.equals(th.getWhereClause());
+                && extendsClause.equals(th.getExtendsClause());
         }
         return false;
     }
