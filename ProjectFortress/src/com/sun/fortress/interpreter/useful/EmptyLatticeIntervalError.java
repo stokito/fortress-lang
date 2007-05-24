7f538a2006f428d85225857b7ef8ca05ff3b5e20
dc12360 <dc12360@localhost>
2007-05-24 17:23:43 +0000

    Oops, forgot to add these.

---
+++ ProjectFortress/src/com/sun/fortress/interpreter/useful/EmptyLatticeIntervalError.java
@@ -0,0 +1,28 @@
+/*
+ * Created on May 22, 2007
+ *
+ */
+package com.sun.fortress.interpreter.useful;
+
+public class EmptyLatticeIntervalError extends Error {
+
+    public EmptyLatticeIntervalError() {
+        // TODO Auto-generated constructor stub
+    }
+
+    public EmptyLatticeIntervalError(String arg0) {
+        super(arg0);
+        // TODO Auto-generated constructor stub
+    }
+
+    public EmptyLatticeIntervalError(Throwable arg0) {
+        super(arg0);
+        // TODO Auto-generated constructor stub
+    }
+
+    public EmptyLatticeIntervalError(String arg0, Throwable arg1) {
+        super(arg0, arg1);
+        // TODO Auto-generated constructor stub
+    }
+
+}
