14a65769d718f6519a46aeabac03664038709f3e
sukyoungryu <sukyoungryu@localhost>
2007-07-10 05:07:54 +0000

    Nodes clean up: AliasedDottedId and AliasedName

--- ProjectFortress/src/com/sun/fortress/interpreter/nodes/AliasedDottedId.java
+++ ProjectFortress/src/com/sun/fortress/interpreter/nodes/AliasedDottedId.java
@@ -17,34 +17,36 @@
 
 package com.sun.fortress.interpreter.nodes;
 
-import com.sun.fortress.interpreter.nodes_util.Span;
-import com.sun.fortress.interpreter.useful.None;
-import com.sun.fortress.interpreter.useful.Option;
+import java.io.IOException;
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+import com.sun.fortress.interpreter.nodes_util.*;
+import com.sun.fortress.interpreter.useful.*;
 
 public class AliasedDottedId extends AbstractNode {
+  private final DottedId _id;
+  private final Option<DottedId> _alias;
 
-    DottedId id;
+  /**
+   * Constructs a AliasedDottedId.
+   * @throws java.lang.IllegalArgumentException  If any parameter to the constructor is null.
+   */
+  public AliasedDottedId(Span in_span, DottedId in_id, Option<DottedId> in_alias) {
+    super(in_span);
 
-    Option<DottedId> alias;
-
-    public AliasedDottedId(Span span, DottedId id, Option<DottedId> alias) {
-        super(span);
-        this.id = id;
-        this.alias = alias;
+    if (in_id == null) {
+      throw new java.lang.IllegalArgumentException("Parameter 'id' to the AliasedDottedId constructor was null");
     }
+    _id = in_id;
 
-    public AliasedDottedId(Span span) {
-        super(span);
-        // TODO Auto-generated constructor stub
+    if (in_alias == null) {
+      throw new java.lang.IllegalArgumentException("Parameter 'alias' to the AliasedDottedId constructor was null");
     }
-
-    public AliasedDottedId(AbstractNode rewriteFrom) {
-        super(rewriteFrom);
-        // TODO Auto-generated constructor stub
-    }
-
-    public DottedId getId() { return id; }
-    public Option<DottedId> getAlias() { return alias; }
+    _alias = in_alias;
+  }
 
     @Override
     public <T> T accept(NodeVisitor<T> v) {
@@ -52,4 +54,96 @@ public class AliasedDottedId extends AbstractNode {
         return null;
     }
 
+    public AliasedDottedId(Span span) {
+        super(span);
+        // TODO Auto-generated constructor stub
+        _id = null;
+        _alias = null;
+    }
+
+  final public DottedId getId() { return _id; }
+  final public Option<DottedId> getAlias() { return _alias; }
+
+  public <RetType> RetType visit(NodeVisitor<RetType> visitor) { return visitor.forAliasedDottedId(this); }
+  public void visit(NodeVisitor_void visitor) { visitor.forAliasedDottedId(this); }
+
+  /**
+   * Implementation of toString that uses
+   * {@link #output} to generate a nicely tabbed tree.
+   */
+  public java.lang.String toString() {
+    java.io.StringWriter w = new java.io.StringWriter();
+    output(w);
+    return w.toString();
+  }
+
+  /**
+   * Prints this object out as a nicely tabbed tree.
+   */
+  public void output(java.io.Writer writer) {
+    outputHelp(new TabPrintWriter(writer, 2), false);
+  }
+
+  protected void outputHelp(TabPrintWriter writer, boolean lossless) {
+    writer.print("AliasedDottedId:");
+    writer.indent();
+
+    Span temp_span = getSpan();
+    writer.startLine();
+    writer.print("span = ");
+    if (lossless) {
+      writer.printSerialized(temp_span);
+      writer.print(" ");
+      writer.printEscaped(temp_span);
+    } else { writer.print(temp_span); }
+
+    DottedId temp_id = getId();
+    writer.startLine();
+    writer.print("id = ");
+    temp_id.outputHelp(writer, lossless);
+
+    Option<DottedId> temp_alias = getAlias();
+    writer.startLine();
+    writer.print("alias = ");
+    if (lossless) {
+      writer.printSerialized(temp_alias);
+      writer.print(" ");
+      writer.printEscaped(temp_alias);
+    } else { writer.print(temp_alias); }
+    writer.unindent();
+  }
+
+  /**
+   * Implementation of equals that is based on the values of the fields of the
+   * object. Thus, two objects created with identical parameters will be equal.
+   */
+  public boolean equals(java.lang.Object obj) {
+    if (obj == null) return false;
+    if ((obj.getClass() != this.getClass()) || (obj.hashCode() != this.hashCode())) {
+      return false;
+    } else {
+      AliasedDottedId casted = (AliasedDottedId) obj;
+      DottedId temp_id = getId();
+      DottedId casted_id = casted.getId();
+      if (!(temp_id == casted_id || temp_id.equals(casted_id))) return false;
+      Option<DottedId> temp_alias = getAlias();
+      Option<DottedId> casted_alias = casted.getAlias();
+      if (!(temp_alias == casted_alias || temp_alias.equals(casted_alias))) return false;
+      return true;
+    }
+  }
+
+  /**
+   * Implementation of hashCode that is consistent with equals.  The value of
+   * the hashCode is formed by XORing the hashcode of the class object with
+   * the hashcodes of all the fields of the object.
+   */
+  protected int generateHashCode() {
+    int code = getClass().hashCode();
+    DottedId temp_id = getId();
+    code ^= temp_id.hashCode();
+    Option<DottedId> temp_alias = getAlias();
+    code ^= temp_alias.hashCode();
+    return code;
+  }
 }
--- ProjectFortress/src/com/sun/fortress/interpreter/nodes/AliasedName.java
+++ ProjectFortress/src/com/sun/fortress/interpreter/nodes/AliasedName.java
@@ -17,45 +17,131 @@
 
 package com.sun.fortress.interpreter.nodes;
 
+import java.io.IOException;
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
 import com.sun.fortress.interpreter.nodes_util.*;
-import com.sun.fortress.interpreter.useful.None;
-import com.sun.fortress.interpreter.useful.Option;
-import com.sun.fortress.interpreter.useful.Some;
+import com.sun.fortress.interpreter.useful.*;
 
 public class AliasedName extends AbstractNode {
+  private final FnName _name;
+  private final Option<FnName> _alias;
 
-    FnName name;
+  /**
+   * Constructs a AliasedName.
+   * @throws java.lang.IllegalArgumentException  If any parameter to the constructor is null.
+   */
+  public AliasedName(Span in_span, FnName in_name, Option<FnName> in_alias) {
+    super(in_span);
 
-    Option<FnName> alias;
-
-    public AliasedName(Span span, FnName name, Option<FnName> alias) {
-        super(span);
-        this.name = name;
-        this.alias = alias;
+    if (in_name == null) {
+      throw new java.lang.IllegalArgumentException("Parameter 'name' to the AliasedName constructor was null");
     }
+    _name = in_name;
 
-    public AliasedName(Span span) {
-        super(span);
-        // TODO Auto-generated constructor stub
+    if (in_alias == null) {
+      throw new java.lang.IllegalArgumentException("Parameter 'alias' to the AliasedName constructor was null");
     }
-
-    public AliasedName(AbstractNode rewriteFrom) {
-        super(rewriteFrom);
-        // TODO Auto-generated constructor stub
-    }
+    _alias = in_alias;
+  }
 
     @Override
     public <T> T accept(NodeVisitor<T> v) {
-        // TODO Auto-generated method stub
-        return null;
+        return v.forAliasedName(this);
     }
 
-    public FnName getName() {
-        return name;
+    AliasedName(Span span) {
+        super(span);
+        _name = null;
+        _alias = null;
     }
 
-    public Option<FnName> getAlias() {
-        return alias;
-    }
+  final public FnName getName() { return _name; }
+  final public Option<FnName> getAlias() { return _alias; }
 
+  public <RetType> RetType visit(NodeVisitor<RetType> visitor) { return visitor.forAliasedName(this); }
+  public void visit(NodeVisitor_void visitor) { visitor.forAliasedName(this); }
+
+  /**
+   * Implementation of toString that uses
+   * {@link #output} to generate a nicely tabbed tree.
+   */
+  public java.lang.String toString() {
+    java.io.StringWriter w = new java.io.StringWriter();
+    output(w);
+    return w.toString();
+  }
+
+  /**
+   * Prints this object out as a nicely tabbed tree.
+   */
+  public void output(java.io.Writer writer) {
+    outputHelp(new TabPrintWriter(writer, 2), false);
+  }
+
+  protected void outputHelp(TabPrintWriter writer, boolean lossless) {
+    writer.print("AliasedName:");
+    writer.indent();
+
+    Span temp_span = getSpan();
+    writer.startLine();
+    writer.print("span = ");
+    if (lossless) {
+      writer.printSerialized(temp_span);
+      writer.print(" ");
+      writer.printEscaped(temp_span);
+    } else { writer.print(temp_span); }
+
+    FnName temp_name = getName();
+    writer.startLine();
+    writer.print("name = ");
+    temp_name.outputHelp(writer, lossless);
+
+    Option<FnName> temp_alias = getAlias();
+    writer.startLine();
+    writer.print("alias = ");
+    if (lossless) {
+      writer.printSerialized(temp_alias);
+      writer.print(" ");
+      writer.printEscaped(temp_alias);
+    } else { writer.print(temp_alias); }
+    writer.unindent();
+  }
+
+  /**
+   * Implementation of equals that is based on the values of the fields of the
+   * object. Thus, two objects created with identical parameters will be equal.
+   */
+  public boolean equals(java.lang.Object obj) {
+    if (obj == null) return false;
+    if ((obj.getClass() != this.getClass()) || (obj.hashCode() != this.hashCode())) {
+      return false;
+    } else {
+      AliasedName casted = (AliasedName) obj;
+      FnName temp_name = getName();
+      FnName casted_name = casted.getName();
+      if (!(temp_name == casted_name || temp_name.equals(casted_name))) return false;
+      Option<FnName> temp_alias = getAlias();
+      Option<FnName> casted_alias = casted.getAlias();
+      if (!(temp_alias == casted_alias || temp_alias.equals(casted_alias))) return false;
+      return true;
+    }
+  }
+
+  /**
+   * Implementation of hashCode that is consistent with equals.  The value of
+   * the hashCode is formed by XORing the hashcode of the class object with
+   * the hashcodes of all the fields of the object.
+   */
+  protected int generateHashCode() {
+    int code = getClass().hashCode();
+    FnName temp_name = getName();
+    code ^= temp_name.hashCode();
+    Option<FnName> temp_alias = getAlias();
+    code ^= temp_alias.hashCode();
+    return code;
+  }
 }
