a487a8348f00197f1dbe6a506d17bb80f7dc7e6d
sukyoungryu <sukyoungryu@localhost>
2007-07-14 10:41:57 +0000

    Added copyright notices.

--- ProjectFortress/src/com/sun/fortress/interpreter/nodes_util/UIDObject.java
+++ ProjectFortress/src/com/sun/fortress/interpreter/nodes_util/UIDObject.java
@@ -1,14 +1,27 @@
-/*
- * Created on Jul 12, 2007
- *
- */
+/*******************************************************************************
+    Copyright 2007 Sun Microsystems, Inc.,
+    4150 Network Circle, Santa Clara, California 95054, U.S.A.
+    All rights reserved.
+
+    U.S. Government Rights - Commercial software.
+    Government users are subject to the Sun Microsystems, Inc. standard
+    license agreement and applicable provisions of the FAR and its supplements.
+
+    Use is subject to license terms.
+
+    This distribution may include materials developed by third parties.
+
+    Sun, Sun Microsystems, the Sun logo and Java are trademarks or registered
+    trademarks of Sun Microsystems, Inc. in the U.S. and other countries.
+ ******************************************************************************/
+
 package com.sun.fortress.interpreter.nodes_util;
 
 public class UIDObject {
 
     /* (non-Javadoc)
      * @see java.lang.Object#hashCode()
-     * 
+     *
      * This is mostly for testing purposes, but it also allows
      * a more deterministic replay than Object.hashCode().
      */
@@ -21,15 +34,15 @@ public class UIDObject {
     static private long seedUID = 0x7b546b0e12fd2559L;
     static private long prevUID = seedUID;
     private transient final long uid;
-    
+
     public UIDObject() {
         uid = next();
     }
-    
+
     public final long getUID() {
         return uid;
     }
-    
+
     /* LFSR generating 63-bit residues */
     private long next() {
         synchronized (lock)
--- ProjectFortress/src/com/sun/fortress/interpreter/unit_tests/ObjectUID_JUTest.java
+++ ProjectFortress/src/com/sun/fortress/interpreter/unit_tests/ObjectUID_JUTest.java
@@ -1,7 +1,20 @@
-/*
- * Created on Jul 12, 2007
- *
- */
+/*******************************************************************************
+    Copyright 2007 Sun Microsystems, Inc.,
+    4150 Network Circle, Santa Clara, California 95054, U.S.A.
+    All rights reserved.
+
+    U.S. Government Rights - Commercial software.
+    Government users are subject to the Sun Microsystems, Inc. standard
+    license agreement and applicable provisions of the FAR and its supplements.
+
+    Use is subject to license terms.
+
+    This distribution may include materials developed by third parties.
+
+    Sun, Sun Microsystems, the Sun logo and Java are trademarks or registered
+    trademarks of Sun Microsystems, Inc. in the U.S. and other countries.
+ ******************************************************************************/
+
 package com.sun.fortress.interpreter.unit_tests;
 
 import junit.framework.Assert;
@@ -24,37 +37,37 @@ public class ObjectUID_JUTest extends TcWrapper {
     }
 
     static class HasUID extends UIDObject {
-        
+
     }
-    
+
     public void testUID() {
         staticTestUID();
     }
-    
+
     static public void staticTestUID() {
         BATree<UIDObject, UIDObject> m = UIDMapFactory.<UIDObject>make();
-        
+
         for (int i = 0; i < 10; i++) {
             HasUID h = new HasUID();
             System.out.println("Sample uid = 0x" + Long.toHexString(h.getUID()));
         }
-        
+
         for (int i = 0; i < 100000; i++) {
             HasUID h = new HasUID();
             Assert.assertTrue("UIDs must be positive", h.getUID() > 0);
             m.put(h, h);
         }
-        
+
         System.err.println("Finished adding to map");
-        
+
         for (UIDObject k : m.keySet()) {
             UIDObject v = m.get(k);
             Assert.assertTrue("UIDS must be unique (two uids maoped to same object", k == v);
         }
     }
-    
+
     public static void main() {
         staticTestUID();
     }
-    
+
 }
