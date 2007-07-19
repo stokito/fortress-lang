5d3f75910b6b00aa753b5fd73cd3534503f73070
dlsmith <dlsmith@localhost>
2007-07-19 16:33:49 +0000

    Updated classpath of scripts to point to plt.jar

--- ProjectFortress/fortress.bat
+++ ProjectFortress/fortress.bat
@@ -15,4 +15,4 @@ REM #    Sun, Sun Microsystems, the Sun logo and Java are trademarks or register
 REM #    trademarks of Sun Microsystems, Inc. in the U.S. and other countries.
 REM ################################################################################
 
-java -cp "build;third_party/junit/junit.jar;third_party/xtc/xtc.jar;third_party/FJ/concurrent.jar;third_party/dstm2/dstm2.jar;third_party/bcel/bcel-5.2.jar" com.sun.fortress.interpreter.drivers.fs %1 %2 %3 %4 %5 %6 %7 %8 %9
+java -cp "build;third_party/junit/junit.jar;third_party/xtc/xtc.jar;third_party/FJ/concurrent.jar;third_party/dstm2/dstm2.jar;third_party/bcel/bcel-5.2.jar;third_party/plt/plt.jar" com.sun.fortress.interpreter.drivers.fs %1 %2 %3 %4 %5 %6 %7 %8 %9
