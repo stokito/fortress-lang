911fa8f776283bb56c7e8c148f743aebbbc61247
sukyoungryu <sukyoungryu@localhost>
2007-08-31 14:41:59 +0000

    Added a missing copyright notice.

--- ProjectFortress/src/com/sun/fortress/interpreter/evaluator/values/GenericNativeConstructor.java
+++ ProjectFortress/src/com/sun/fortress/interpreter/evaluator/values/GenericNativeConstructor.java
@@ -1,7 +1,20 @@
-/*
- * Created on Aug 30, 2007
- *
- */
+/*******************************************************************************
+    Copyright 2007 Sun Microsystems, Inc.,
+    4150 Network Circle, Santa Clara, California 95054, U.S.A.
+    All rights reserved.
+
+    U.S. Government Rights - Commercial software.
+    Government users are subject to the Sun Microsystems, Inc. standard
+    license agreement and applicable provisions of the FAR and its supplements.
+
+    Use is subject to license terms.
+
+    This distribution may include materials developed by third parties.
+
+    Sun, Sun Microsystems, the Sun logo and Java are trademarks or registered
+    trademarks of Sun Microsystems, Inc. in the U.S. and other countries.
+ ******************************************************************************/
+
 package com.sun.fortress.interpreter.evaluator.values;
 
 import java.util.List;
@@ -15,7 +28,7 @@ import com.sun.fortress.nodes.GenericWithParams;
 public class GenericNativeConstructor extends GenericConstructor {
 
     private String name;
-    
+
     public GenericNativeConstructor(Environment env,
             GenericWithParams odefOrDecl, String name) {
         super(env, odefOrDecl);
