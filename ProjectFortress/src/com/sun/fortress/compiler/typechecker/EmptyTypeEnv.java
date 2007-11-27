e6e5a5cafb2a4cbff61db4aa4adaf7f2abf83baf
EricAllen <EricAllen@localhost>
2007-11-27 23:06:06 +0000

    Moved type checker code to its own package. Added simple type environments.

---
+++ ProjectFortress/src/com/sun/fortress/compiler/typechecker/EmptyTypeEnv.java
@@ -0,0 +1,32 @@
+/*******************************************************************************
+    Copyright 2007 Sun Microsystems, Inc.,
+    4150 Network Circle, Santa Clara, California 95054, U.S.A.
+    All rights reserved.
+
+    U.S. Government Rights - Commercial software.
+    Government users are subject to the Sun Microsystems, Inc. standard
+    license agreement and applicable provisions of the FAR and its supplements.
+
+    Use is subject to license terms.
+
+    This distribution may include materials developed by third parties.
+
+    Sun, Sun Microsystems, the Sun logo and Java are trademarks or registered
+    trademarks of Sun Microsystems, Inc. in the U.S. and other countries.
+ ******************************************************************************/
+
+package com.sun.fortress.compiler.typechecker;
+
+import com.sun.fortress.nodes.*;
+import java.util.*;
+
+class EmptyTypeEnv extends TypeEnv {
+    public static final EmptyTypeEnv ONLY = new EmptyTypeEnv();
+    
+    private EmptyTypeEnv() {}
+    
+    private RuntimeException error() { throw new RuntimeException("Attempt to lookup in an EmptyTypeEnv."); }
+    public Type type(IdName var) { throw error(); }
+    public List<Modifier> mods(IdName var) { throw error(); }
+    public boolean mutable(IdName var) { throw error(); }
+}
