d7f38bcd15d7691c8d758629c23da8d44dc9aa52
EricAllen <EricAllen@localhost>
2008-01-07 22:02:13 +0000

    Added another type environment variant for handling declared function parameters.

---
+++ ProjectFortress/src/com/sun/fortress/compiler/typechecker/ParamTypeEnv.java
@@ -0,0 +1,56 @@
+/*******************************************************************************
+    Copyright 2007 Sun Microsystems, Inc.,
+    4150 Network Circle, Santa Clara, California 95054, U.S.A.
+    All rights reserved.
+
+    U.S. Government Rights - Commercial software.
+    Government users are subject to the Sun Microsystems, Inc. standard
+    license agreement and applicable provisions of the FAR and its supplements.
+
+    Use is subject to license terms.
+
+    This distribution may include materials developed by third parties.
+
+    Sun, Sun Microsystems, the Sun logo and Java are trademarks or registered
+    trademarks of Sun Microsystems, Inc. in the U.S. and other countries.
+ ******************************************************************************/
+
+package com.sun.fortress.compiler.typechecker;
+
+import com.sun.fortress.compiler.*;
+import com.sun.fortress.compiler.index.*;
+import com.sun.fortress.nodes.*;
+import com.sun.fortress.nodes_util.NodeFactory;
+import edu.rice.cs.plt.collect.Relation;
+import edu.rice.cs.plt.tuple.Option;
+import java.util.*;
+
+import static com.sun.fortress.nodes_util.NodeFactory.*;
+import static edu.rice.cs.plt.tuple.Option.*;
+
+/** 
+ * A type environment whose outermost lexical scope consists of a map from IDs
+ * to Variables.
+ */
+class ParamTypeEnv extends TypeEnv {
+    private List<Param> entries;
+    private TypeEnv parent;
+    
+    ParamTypeEnv(List<Param> _entries, TypeEnv _parent) {
+        entries = _entries;
+        parent = _parent;
+    }
+    
+    /**
+     * Return an LValueBind that binds the given Id to a type
+     * (if the given Id is in this type environment).
+     */
+    public Option<LValueBind> binding(Id var) {
+        for (Param param: entries) {
+            if (param.getName().equals(var)) {
+                some(makeLValue(var, typeFromParam(param)));
+            }
+        }
+        return parent.binding(var);
+    }
+}
