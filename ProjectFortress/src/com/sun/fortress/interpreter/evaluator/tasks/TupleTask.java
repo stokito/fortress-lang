b342927a751da704f6e6ee3f5d0cb49452c54f92
sukyoungryu <sukyoungryu@localhost>
2008-02-16 13:21:53 +0000

    Fixed copyright notices and did some clean up.

--- ProjectFortress/src/com/sun/fortress/interpreter/evaluator/tasks/TupleTask.java
+++ ProjectFortress/src/com/sun/fortress/interpreter/evaluator/tasks/TupleTask.java
@@ -1,5 +1,5 @@
 /********************************************************************************
-  Copyright 2007 Sun Microsystems, Inc.,
+  Copyright 2008 Sun Microsystems, Inc.,
   4150 Network Circle, Santa Clara, California 95054, U.S.A.
   All rights reserved.
 
