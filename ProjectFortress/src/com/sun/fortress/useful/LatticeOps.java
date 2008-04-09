b4367943e4be3e70886862e21adf2948618946df
sukyoungryu <sukyoungryu@localhost>
2008-04-09 13:34:00 +0000

    Fixed copyright notices.

--- ProjectFortress/src/com/sun/fortress/useful/LatticeOps.java
+++ ProjectFortress/src/com/sun/fortress/useful/LatticeOps.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
-    Copyright 2007 Sun Microsystems, Inc.,
+    Copyright 2008 Sun Microsystems, Inc.,
     4150 Network Circle, Santa Clara, California 95054, U.S.A.
     All rights reserved.
 
@@ -23,33 +23,33 @@ public interface LatticeOps<U> {
      * rather than its dual sense.
      **/
     boolean isForward();
-    
+
     /**
      * Returns bottom element of lattice.
      * Bottom, least general, empty set.
      */
     U zero();
-    
+
     /**
      * Returns top element of lattice.
      * Top, most general, set of all.
      */
     U one();
-   
+
     /**
      * Meet, Square Cap, Greatest Lower Bound, Infimum, more specific, intersection.
-     * 
+     *
      * lattice.meet(lattice.one(), X) == X.
      */
     U meet(U x, U y);
-    
+
     /**
      * Join, Square Cup, Least Upper Bound, Supremum, more general, union.
-     * 
+     *
      * lattice.join(lattice.zero(), X) == X.
      */
-    
+
     U join(U x, U y);
-    
+
     LatticeOps<U> dual();
 }
