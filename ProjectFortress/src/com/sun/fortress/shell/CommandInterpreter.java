a8d663fb224317c0ebf34d7dcdfb0c38d19bb6c3
jdn <jdn@localhost>
2008-05-28 11:44:46 +0000

    [Xml] Added quotes arround attributes

--- ProjectFortress/src/com/sun/fortress/shell/CommandInterpreter.java
+++ ProjectFortress/src/com/sun/fortress/shell/CommandInterpreter.java
@@ -37,190 +37,189 @@ import static com.sun.fortress.interpreter.evaluator.InterpreterBug.bug;
 import java.io.*;
 
 public class CommandInterpreter {
- private Shell shell;
+    private Shell shell;
 
-// public static boolean debug;
- boolean test;
- boolean nolib;
+    // public static boolean debug;
+    boolean test;
+    boolean nolib;
 
- CommandInterpreter(Shell _shell) {
-  shell = _shell;
- }
+    CommandInterpreter(Shell _shell) {
+        shell = _shell;
+    }
 
- /**
-  * This compiler uses a different method for determining what to compile,
-  * and how to compile it.
-  *
-  * @param doLink
-  * @param s
-  * @throws UserError
-  * @throws InterruptedException
-  * @throws IOException
-  */
- void compile(boolean doLink, String s) throws UserError, InterruptedException, IOException {
-  try {
-   //FortressRepository fileBasedRepository = new FileBasedRepository(shell.getPwd());
-   Fortress fortress = new Fortress(new CacheBasedRepository(ProjectProperties.ANALYZED_CACHE_DIR));
+    /**
+     * This compiler uses a different method for determining what to compile,
+     * and how to compile it.
+     *
+     * @param doLink
+     * @param s
+     * @throws UserError
+     * @throws InterruptedException
+     * @throws IOException
+     */
+    void compile(boolean doLink, String s) throws UserError, InterruptedException, IOException {
+        try {
+            //FortressRepository fileBasedRepository = new FileBasedRepository(shell.getPwd());
+            Fortress fortress = new Fortress(new CacheBasedRepository(ProjectProperties.ANALYZED_CACHE_DIR));
 
-   Path path = ProjectProperties.SOURCE_PATH;
+            Path path = ProjectProperties.SOURCE_PATH;
 
-   if (s.contains("/")) {
-    String head = s.substring(0, s.lastIndexOf("/"));
-    s = s.substring(s.lastIndexOf("/")+1, s.length());
-    path = path.prepend(head);
-   }
+            if (s.contains("/")) {
+                String head = s.substring(0, s.lastIndexOf("/"));
+                s = s.substring(s.lastIndexOf("/")+1, s.length());
+                path = path.prepend(head);
+            }
 
-   Iterable<? extends StaticError> errors = fortress.compile(path, s);
+            Iterable<? extends StaticError> errors = fortress.compile(path, s);
 
-   for (StaticError error: errors) {
-    System.err.println(error);
-   }
-   // If there are no errors, all components will have been written to disk by the FileBasedRepository.
-  }
-  catch (RepositoryError error) {
-   System.err.println(error);
-  }
- }
+            for (StaticError error: errors) {
+                System.err.println(error);
+            }
+            // If there are no errors, all components will have been written to disk by the FileBasedRepository.
+        }
+        catch (RepositoryError error) {
+            System.err.println(error);
+        }
+    }
 
- /* Upgrade the internal files of the resident fortress with the contents of the given tar file.*/
- void selfUpgrade(String fileName) throws UserError, InterruptedException {
-  //Ant.run("selfupgrade", "-Dtarfile=" + fileName);
- }
+    /* Upgrade the internal files of the resident fortress with the contents of the given tar file.*/
+    void selfUpgrade(String fileName) throws UserError, InterruptedException {
+        //Ant.run("selfupgrade", "-Dtarfile=" + fileName);
+    }
 
- /* Convenience method for calling selfUpgrade directly with a file.*/
- void selfUpgrade(File file) throws UserError, InterruptedException { selfUpgrade(file.getPath()); }
+    /* Convenience method for calling selfUpgrade directly with a file.*/
+    void selfUpgrade(File file) throws UserError, InterruptedException { selfUpgrade(file.getPath()); }
 
- /* Checks whether a component or API has been installed in the resident fortress.*/
- void exists(String componentName) throws UserError {
-  if (isInstalled(componentName)) {
-   System.out.println("Yes, component " + componentName + " is installed in this fortress.");
-  }
-  else if (new File(shell.getComponents() + SEP + "apis" + SEP + componentName + SEP + ".jst").exists()) {
-   System.out.println("Yes, API " + componentName + " is installed in this fortress.");
-  }
-  else {
-   System.out.println("No, there is no component or API with name " + componentName + " installed in this fortress.");
-  }
- }
+    /* Checks whether a component or API has been installed in the resident fortress.*/
+    void exists(String componentName) throws UserError {
+        if (isInstalled(componentName)) {
+            System.out.println("Yes, component " + componentName + " is installed in this fortress.");
+        }
+        else if (new File(shell.getComponents() + SEP + "apis" + SEP + componentName + SEP + ".jst").exists()) {
+            System.out.println("Yes, API " + componentName + " is installed in this fortress.");
+        }
+        else {
+            System.out.println("No, there is no component or API with name " + componentName + " installed in this fortress.");
+        }
+    }
 
- /* Runs a fortress source file directly.*/
- void script(String fileName) throws UserError, IOException {
-     Option<CompilationUnit> cu = makeCompilationUnit(fileName);
-     if (cu.isSome())
-         Driver.evalComponent(cu.unwrap(),
-                              new PathBasedRepository(ProjectProperties.SOURCE_PATH));
-     else bug("Failed to make a compilation unit for " + fileName);
- }
+    /* Runs a fortress source file directly.*/
+    void script(String fileName) throws UserError, IOException {
+        Option<CompilationUnit> cu = makeCompilationUnit(fileName);
+        if (cu.isSome())
+            Driver.evalComponent(cu.unwrap(),
+                    new PathBasedRepository(ProjectProperties.SOURCE_PATH));
+        else bug("Failed to make a compilation unit for " + fileName);
+    }
 
- void run(List<String> args) throws UserError, IOException, Throwable {
-  if (args.size() == 0) {
-   throw new UserError("Need a file to run");
-  }
-  String s = args.get(0);
-  List<String> rest = args.subList(1, args.size());
+    void run(List<String> args) throws UserError, IOException, Throwable {
+        if (args.size() == 0) {
+            throw new UserError("Need a file to run");
+        }
+        String s = args.get(0);
+        List<String> rest = args.subList(1, args.size());
 
-  if (s.startsWith("-")) {
-   if (s.equals("-debug")) ProjectProperties.debug = true;
-   if (s.equals("-test")) test= true;
-   if (s.equals("-nolib")) nolib= true;
-   if (s.equals("-noPreparse")) ProjectProperties.noPreparse = true;
-   run(rest);
-  } else {
-   run(s, rest);
-  }
- }
- void run(String fileName, List<String> args) throws UserError, IOException, Throwable {
-  try {
-   //FortressRepository fileBasedRepository = new FileBasedRepository(shell.getPwd());
-   Fortress fortress = new Fortress(new CacheBasedRepository(ProjectProperties.ANALYZED_CACHE_DIR));
+        if (s.startsWith("-")) {
+            if (s.equals("-debug")) ProjectProperties.debug = true;
+            if (s.equals("-test")) test= true;
+            if (s.equals("-nolib")) nolib= true;
+            if (s.equals("-noPreparse")) ProjectProperties.noPreparse = true;
+            run(rest);
+        } else {
+            run(s, rest);
+        }
+    }
+    void run(String fileName, List<String> args) throws UserError, IOException, Throwable {
+        try {
+            //FortressRepository fileBasedRepository = new FileBasedRepository(shell.getPwd());
+            Fortress fortress = new Fortress(new CacheBasedRepository(ProjectProperties.ANALYZED_CACHE_DIR));
 
-   Path path = ProjectProperties.SOURCE_PATH;
+            Path path = ProjectProperties.SOURCE_PATH;
 
-   if (fileName.contains("/")) {
-    String head = fileName.substring(0, fileName.lastIndexOf("/"));
-    fileName = fileName.substring(fileName.lastIndexOf("/")+1, fileName.length());
-    path = path.prepend(head);
-   }
+            if (fileName.contains("/")) {
+                String head = fileName.substring(0, fileName.lastIndexOf("/"));
+                fileName = fileName.substring(fileName.lastIndexOf("/")+1, fileName.length());
+                path = path.prepend(head);
+            }
 
-   /*
-    * Strip suffix to get a bare component name.
-    */
-   if (fileName.endsWith("." + ProjectProperties.COMP_SOURCE_SUFFIX)) {
-    fileName = fileName.substring(0, fileName.length() - ProjectProperties.COMP_SOURCE_SUFFIX.length());
-   }
+            /*
+             * Strip suffix to get a bare component name.
+             */
+            if (fileName.endsWith("." + ProjectProperties.COMP_SOURCE_SUFFIX)) {
+                fileName = fileName.substring(0, fileName.length() - ProjectProperties.COMP_SOURCE_SUFFIX.length());
+            }
 
-   Iterable<? extends StaticError> errors = fortress.run(path, fileName, test, nolib, args);
+            Iterable<? extends StaticError> errors = fortress.run(path, fileName, test, nolib, args);
 
-   for (StaticError error: errors) {
-    System.err.println(error);
-   }
-   // If there are no errors, all components will have been written to disk by the FileBasedRepository.
-  }
-  catch (RepositoryError e) {
-   System.err.println(e.getMessage());
-  }
-  catch (FortressException e) {
-   System.err.println(e.getMessage());
-   e.printInterpreterStackTrace(System.err);
+            for (StaticError error: errors) {
+                System.err.println(error);
+            }
+            // If there are no errors, all components will have been written to disk by the FileBasedRepository.
+        }
+        catch (RepositoryError e) {
+            System.err.println(e.getMessage());
+        }
+        catch (FortressException e) {
+            System.err.println(e.getMessage());
+            e.printInterpreterStackTrace(System.err);
+            if (ProjectProperties.debug) {
+                e.printStackTrace();
+            } else {
+                System.err.println("Turn on -debug for Java-level error dump.");
+            }
+            System.exit(1);
+        }
+    }
 
-   if (ProjectProperties.debug) {
-    e.printStackTrace();
-   } else {
-    System.err.println("Turn on -debug for Java-level error dump.");
-   }
-   System.exit(1);
-  }
- }
+    void link(String result, String left, String right) throws UserError { throw new UserError("Error: Link not yet implemented!"); }
+    void upgrade(String result, String left, String right) throws UserError { throw new UserError("Error: Upgrade not yet implemented!"); }
 
- void link(String result, String left, String right) throws UserError { throw new UserError("Error: Link not yet implemented!"); }
- void upgrade(String result, String left, String right) throws UserError { throw new UserError("Error: Upgrade not yet implemented!"); }
+    void api(String fileName) throws IOException, UserError {
+        FileBasedRepository fileBasedRepository = new FileBasedRepository(shell.getPwd());
+        Fortress fortress = new Fortress(fileBasedRepository);
 
- void api(String fileName) throws IOException, UserError {
-  FileBasedRepository fileBasedRepository = new FileBasedRepository(shell.getPwd());
-  Fortress fortress = new Fortress(fileBasedRepository);
+        File file = new File(fileName);
+        Iterable<Component> _components = Parser.parse(file).components();
 
-  File file = new File(fileName);
-  Iterable<Component> _components = Parser.parse(file).components();
+        // Compile to ensure there are no static errors.
+        // Disable until static checking is actually working.
+        // Iterable<? extends StaticError> errors = fortress.compile(file);
 
-  // Compile to ensure there are no static errors.
-  // Disable until static checking is actually working.
-  // Iterable<? extends StaticError> errors = fortress.compile(file);
+        //        if (errors.iterator().hasNext()) {
+        //            for (StaticError error: errors) { System.err.println(error); }
+        //        } else {
+        // If there are no errors, all components will have been written to disk by the FileBasedRepository.
+        // We also need to write the corresponding APIs.
+        for (Component component: _components) {
+            Api corresponding = (Api)component.accept(ApiMaker.ONLY);
+            IndexBuilder.ApiResult result = IndexBuilder.buildApis(file.lastModified(), corresponding);
 
-  //        if (errors.iterator().hasNext()) {
-  //            for (StaticError error: errors) { System.err.println(error); }
-  //        } else {
-  // If there are no errors, all components will have been written to disk by the FileBasedRepository.
-  // We also need to write the corresponding APIs.
-  for (Component component: _components) {
-   Api corresponding = (Api)component.accept(ApiMaker.ONLY);
-   IndexBuilder.ApiResult result = IndexBuilder.buildApis(file.lastModified(), corresponding);
+            if (result.isSuccessful()) {
+                fileBasedRepository.addApis(result.apis());
+            } else {
+                for (StaticError error: result.errors()) { System.err.println(error); }
+            }
+        }
+        //        }
+    }
 
-   if (result.isSuccessful()) {
-    fileBasedRepository.addApis(result.apis());
-   } else {
-    for (StaticError error: result.errors()) { System.err.println(error); }
-   }
-  }
-  //        }
- }
+    /* Helper method that creates a CompilationUnit from a Fortress source file name.*/
+    private Option<CompilationUnit> makeCompilationUnit(String fileName) throws UserError {
+        File sourceFile = new File(fileName);
 
- /* Helper method that creates a CompilationUnit from a Fortress source file name.*/
- private Option<CompilationUnit> makeCompilationUnit(String fileName) throws UserError {
-  File sourceFile = new File(fileName);
+        if (! sourceFile.exists()) {
+            throw new UserError("Error: File " + fileName + " does not exist.");
+        }
+        try {
+            return ASTIO.parseToJavaAst(fileName);
+        }
+        catch (IOException e) {
+            throw new ShellException(e);
+        }
+    }
 
-  if (! sourceFile.exists()) {
-   throw new UserError("Error: File " + fileName + " does not exist.");
-  }
-  try {
-   return ASTIO.parseToJavaAst(fileName);
-  }
-  catch (IOException e) {
-   throw new ShellException(e);
-  }
- }
-
- /* Helper method that returns true if a component of the given name is installed.*/
- private boolean isInstalled(String componentName) {
-  return new File(shell.getComponents() + SEP + "components" + SEP + componentName + SEP + ".jst").exists();
- }
+    /* Helper method that returns true if a component of the given name is installed.*/
+    private boolean isInstalled(String componentName) {
+        return new File(shell.getComponents() + SEP + "components" + SEP + componentName + SEP + ".jst").exists();
+    }
 }
