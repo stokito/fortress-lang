ef68d5537311973f80c19a47f4682523b2e95cae
sukyoungryu <sukyoungryu@localhost>
2008-05-29 02:49:51 +0000

    [copyrights] Fixed copyright notices.

--- ProjectFortress/src/com/sun/fortress/nodes_util/SourceLocRats.java
+++ ProjectFortress/src/com/sun/fortress/nodes_util/SourceLocRats.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
-    Copyright 2007 Sun Microsystems, Inc.,
+    Copyright 2008 Sun Microsystems, Inc.,
     4150 Network Circle, Santa Clara, California 95054, U.S.A.
     All rights reserved.
 
@@ -26,7 +26,7 @@ public class SourceLocRats extends SourceLoc {
     int line;
 
     int offset;
-    
+
     String fileName;
 
     @Override
@@ -75,7 +75,7 @@ public class SourceLocRats extends SourceLoc {
         this.col = c;
         this.offset = offset;
     }
-    
+
     public int getOffset() {
     	return offset;
     }
