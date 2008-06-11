7ae2eec2e9a73e01664ae80f83e723da8b9fb967
jdn <jdn@localhost>
2008-06-11 12:51:04 +0000

    [Syntax] added default case in BaseTypeCollector

--- ProjectFortress/src/com/sun/fortress/syntax_abstractions/util/BaseTypeCollector.java
+++ ProjectFortress/src/com/sun/fortress/syntax_abstractions/util/BaseTypeCollector.java
@@ -5,6 +5,11 @@ import com.sun.fortress.nodes.*;
 public class BaseTypeCollector extends NodeDepthFirstVisitor<String> {
 
     @Override
+    public String defaultCase(Node that) {
+        return "";
+    }
+
+    @Override
     public String forVarType(VarType that) {
         return that.getName().getText();   
     }
