c21d6f66e89b1eb41e5e664090c14b1bffac66d7
mspiegel <mspiegel@localhost>
2008-06-12 20:24:02 +0000

    Code commenting of TopLevelEnvGenerator.java

--- ProjectFortress/src/com/sun/fortress/compiler/TopLevelEnvGenerator.java
+++ ProjectFortress/src/com/sun/fortress/compiler/TopLevelEnvGenerator.java
@@ -4,34 +4,49 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.HashSet;
 import java.util.Map;
 
 import org.objectweb.asm.ClassWriter;
-import org.objectweb.asm.MethodVisitor;
 import org.objectweb.asm.Opcodes;
 
 import com.sun.fortress.compiler.index.ComponentIndex;
-import com.sun.fortress.compiler.typechecker.TypeCheckerResult;
 import com.sun.fortress.nodes.APIName;
-import com.sun.fortress.nodes.Component;
 import com.sun.fortress.nodes.Id;
 import com.sun.fortress.nodes.IdOrOpOrAnonymousName;
 import com.sun.fortress.nodes_util.NodeUtil;
 
-import edu.rice.cs.plt.iter.IterUtil;
 
 public class TopLevelEnvGenerator {
-	private static final String FVALUE_SUFFIX = "$FValue";
-	private static final String FTYPE_SUFFIX = "$FType";
+	
+	
+	/**
+	 * From the Fortress Language Specification Version 1.0, Section 7.2:
+	 * 
+	 * 	"Fortress supports three namespaces, one for types, one for values,
+	 *  and one for labels. (If we consider the Fortress component system, 
+	 *  there is another namespace for APIs.) These namespaces are logically 
+	 *  disjoint: names in one namespace do not conﬂict with names in another." 
+	 */
+	private static final String FTYPE_NAMESPACE = "$FType";
+	private static final String FVALUE_NAMESPACE = "$FValue";
+	
 	private static final String FVALUE_DESCRIPTOR = "Lcom/sun/fortress/interpreter/evaluator/values/FValue;";
 	private static final String FTYPE_DESCRIPTOR = "Lcom/sun/fortress/interpreter/evaluator/types/FType;";
 	
+	private static final String CLASSNAME_SUFFIX = "Env";
+	
+	
+	/**
+	 * Given a list of components, generate a Java bytecode compiled environment
+	 * for each component, and dump that compiled class to the filesystem.
+	 * @param components
+	 * @param env
+	 */
 	public static void generate(Map<APIName, ComponentIndex> components,
 			                    GlobalEnvironment env) {
 		for(APIName componentName : components.keySet()) {
 			String className = NodeUtil.nameString(componentName);
-			className = className + "Environment";
+			className = className + CLASSNAME_SUFFIX;
 			
 			byte[] envClass = generateForComponent(className,
 					              components.get(componentName), env);
@@ -39,36 +54,49 @@ public class TopLevelEnvGenerator {
 		}
 	}
 
-	private static byte[] generateForComponent(String name,
+	/**
+	 * Given one component, generate a Java bytecode compiled environment
+	 * for that component.
+	 * @param className
+	 * @param componentIndex
+	 * @param env
+	 * @return
+	 */
+	private static byte[] generateForComponent(String className,
 			                                   ComponentIndex componentIndex,
 			                                   GlobalEnvironment env) {
 		ClassWriter cw = new ClassWriter(0);
-        cw.visit(Opcodes.V1_5, Opcodes.ACC_PUBLIC + Opcodes.ACC_FINAL, name, null, "java/lang/Object", null);
-        // cw.visitField(Opcodes.ACC_PUBLIC + Opcodes.ACC_STATIC, "$foo.bar", "I", null, new Integer(7)).visitEnd();
-        // MethodVisitor mv = cw.visitMethod(Opcodes.ACC_PUBLIC + Opcodes.ACC_STATIC, 
-        //		                                     "main", "([Ljava/lang/String;)V", null, null);
-        
+        cw.visit(Opcodes.V1_5, Opcodes.ACC_PUBLIC + Opcodes.ACC_FINAL, className, null, "java/lang/Object", null);
+
+        // Create all variables as fields in the environment
         for(Id id : componentIndex.variables().keySet()) {
-        	String idString = NodeUtil.nameString(id) + FVALUE_SUFFIX;
+        	String idString = NodeUtil.nameString(id) + FVALUE_NAMESPACE;
             cw.visitField(Opcodes.ACC_PUBLIC, idString, FVALUE_DESCRIPTOR, null, null).visitEnd();	
         }
         
+        // Create all functions as fields in the environment
         for(IdOrOpOrAnonymousName id : componentIndex.functions().firstSet()) {
-        	String idString = NodeUtil.nameString(id) + FVALUE_SUFFIX;
+        	String idString = NodeUtil.nameString(id) + FVALUE_NAMESPACE;
             cw.visitField(Opcodes.ACC_PUBLIC, idString, FVALUE_DESCRIPTOR, null, null).visitEnd();	
         }
-        
+
+        // Create all types as fields in the environment
         for(Id id : componentIndex.typeConses().keySet()) {
-        	String idString = NodeUtil.nameString(id) + FTYPE_SUFFIX;
+        	String idString = NodeUtil.nameString(id) + FTYPE_NAMESPACE;
             cw.visitField(Opcodes.ACC_PUBLIC, idString, FTYPE_DESCRIPTOR, null, null).visitEnd();	
         }
-        
-        
+               
         cw.visitEnd();
         
         return(cw.toByteArray());
 	}
-	
+
+	/**
+	 * Given a Java bytecode class stored in a byte arrray, save that
+	 * class into a file on disk.
+	 * @param bytecode
+	 * @param fileName
+	 */
 	private static void outputClassFile(byte[] bytecode, String fileName) {
 		FileOutputStream outStream;
 		try {
