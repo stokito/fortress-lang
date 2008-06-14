34309a674c89eefa65974000683d97afd18d20ae
dr2chase <dr2chase@localhost>
2008-06-14 04:19:29 +0000

    More BetterEnv cleanup

--- ProjectFortress/src/com/sun/fortress/interpreter/evaluator/BaseEnv.java
+++ ProjectFortress/src/com/sun/fortress/interpreter/evaluator/BaseEnv.java
@@ -21,6 +21,7 @@ import java.io.IOException;
 import java.util.Iterator;
 
 import com.sun.fortress.exceptions.CircularDependenceError;
+import com.sun.fortress.exceptions.RedefinitionError;
 import com.sun.fortress.interpreter.env.IndirectionCell;
 import com.sun.fortress.interpreter.env.ReferenceCell;
 import com.sun.fortress.interpreter.evaluator.scopes.SApi;
@@ -28,8 +29,13 @@ import com.sun.fortress.interpreter.evaluator.scopes.SComponent;
 import com.sun.fortress.interpreter.evaluator.types.FType;
 import com.sun.fortress.interpreter.evaluator.types.FTypeTop;
 import com.sun.fortress.interpreter.evaluator.values.Closure;
+import com.sun.fortress.interpreter.evaluator.values.FBool;
+import com.sun.fortress.interpreter.evaluator.values.FInt;
 import com.sun.fortress.interpreter.evaluator.values.FString;
 import com.sun.fortress.interpreter.evaluator.values.FValue;
+import com.sun.fortress.interpreter.evaluator.values.Fcn;
+import com.sun.fortress.interpreter.evaluator.values.OverloadedFunction;
+import com.sun.fortress.interpreter.evaluator.values.SingleFcn;
 import com.sun.fortress.nodes.APIName;
 import com.sun.fortress.nodes.Id;
 import com.sun.fortress.nodes_util.NodeUtil;
@@ -67,6 +73,112 @@ abstract public class BaseEnv implements Environment {
 
     abstract public  Appendable dump(Appendable a) throws IOException;
 
+    protected final void putNoShadow(String index, FValue value, String what) {
+        FValue fvo = getValueRaw(index);
+        if (fvo == null) {
+            putValueRaw(index, value);
+        } else  if (fvo instanceof IndirectionCell) {
+         // TODO Need to push the generic type Result into IndirectionCell, etc.
+            // This yucky code is "correct" because IndirectionCell extends FValue,
+            // and "it happens to be true" that this code will never be instantiated
+            // above or below FValue in the type hierarchy.
+            // Strictly speaking, this might be wrong if it permits
+            // = redefinition of a mutable cell (doesn't seem to).
+            IndirectionCell ic = (IndirectionCell) fvo;
+            if (ic instanceof ReferenceCell) {
+                throw new RedefinitionError("Mutable variable", index, fvo, value);
+            } else {
+                ic.storeValue((FValue) value);
+            }
+        } else {
+            throw new RedefinitionError(what, index, fvo, value);
+        }
+    }
+
+    protected final  void putFunction(String index, Fcn value, String what, boolean shadowIfDifferent, boolean overloadIsOK) {
+        FValue fvo = getValueRaw(index);
+        if (fvo == null) {
+            putValueRaw(index, value);
+            noteName(index);
+        } else {
+                  if (fvo instanceof IndirectionCell) {
+                    // TODO Need to push the generic type Result into IndirectionCell, etc.
+                    // This yucky code is "correct" because IndirectionCell extends FValue,
+                    // and "it happens to be true" that this code will never be instantiated
+                    // above or below FValue in the type hierarchy.
+                    // Strictly speaking, this might be wrong if it permits
+                    // = redefinition of a mutable cell (doesn't seem to).
+                    IndirectionCell ic = (IndirectionCell) fvo;
+                    if (ic instanceof ReferenceCell) {
+                        throw new RedefinitionError("Mutable variable", index, fvo, value);
+                    } else if (! ic.isInitialized()) {
+                        ic.storeValue((FValue) value);
+                        return;
+                    } else {
+                        // ic is an initialized value cell, not a true function.
+                        // do not overload.
+                        throw new RedefinitionError(what, index, fvo, value);
+                    }
+                }
+
+                /* ic is a function, do an overloading on it.
+                 * Because of wholesale symbol import via linking,
+                 * it is possible to combine a pair of overloadings.
+                 *
+                 * This is all going to get simpler in the future,
+                 * when overloading gets more complicated (allowing mixed
+                 * generic and non-generic overloading).
+                 */
+
+                if (!(fvo instanceof Fcn))
+                    System.err.println("Eek!");
+                Fcn fcn_fvo = (Fcn) fvo;
+                if (! shadowIfDifferent && // true for functional methods
+                    fcn_fvo.getWithin() != value.getWithin() &&
+                    ! (fcn_fvo.getWithin().isTopLevel() &&  value.getWithin().isTopLevel()) // for imports from another api
+                    ) {
+                    /*
+                     * If defined in a different environment, shadow
+                     * instead of overloading.
+                     */
+                    putValueRaw(index, value);
+                    noteName(index);
+                    return ;
+                }
+
+                /*
+                 * Lots of overloading combinations
+                 */
+                OverloadedFunction ovl = null;
+                if (fvo instanceof SingleFcn) {
+                    SingleFcn gm = (SingleFcn)fvo;
+                    ovl = new OverloadedFunction(gm.getFnName(), this);
+                    ovl.addOverload(gm);
+                    putValueRaw(index, ovl);
+                    
+                } else if (fvo instanceof OverloadedFunction) {
+                    ovl = (OverloadedFunction)fvo;
+                } else {
+                    throw new RedefinitionError(what, index,
+                                               fvo, value);
+                }
+                if (value instanceof SingleFcn) {
+                    ovl.addOverload((SingleFcn) value, overloadIsOK);
+                } else if (value instanceof OverloadedFunction) {
+                    ovl.addOverloads((OverloadedFunction) value);
+                } else {
+                    error(errorMsg("Overload of ", ovl,
+                                   " with inconsistent ", value));
+                }
+                /*
+                 * The overloading occurs in the original table, unless a new overload
+                 * was created (see returns of "table.add" above).
+                 */
+               
+            } 
+        
+    }
+ 
     public void assignValue(HasAt loc, String str, FValue value) {
         FValue v = getValueRaw(str);
         if (v instanceof ReferenceCell) {
@@ -315,6 +427,21 @@ abstract public class BaseEnv implements Environment {
 
     public Environment genericLeafEnvHack(Environment genericEnv, HasAt loc) {
         return extend(genericEnv);
+    }
+
+    public void putBool(String str, Boolean f2) {
+        putBoolRaw(str, f2);
+        putValueRaw(str, FBool.make(f2));
+   }
+
+    public void putNat(String str, Number f2) {
+        putNatRaw(str, f2);
+        putValueRaw(str, FInt.make(f2.intValue()));
+    }
+
+    public void putInt(String str, Number f2) {
+        putIntRaw(str, f2);
+        putValueRaw(str, FInt.make(f2.intValue()));
     }
 
 
