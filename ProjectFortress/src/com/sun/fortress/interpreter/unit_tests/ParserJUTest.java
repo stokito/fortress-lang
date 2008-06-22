906ea67084303d352b2de3d55de8cd4ff50d38e5
sukyoungryu <sukyoungryu@localhost>
2008-06-22 17:57:33 +0000

    [tests] Moved unit test files to the unit_tests directory.

--- ProjectFortress/src/com/sun/fortress/interpreter/unit_tests/ParserJUTest.java
+++ ProjectFortress/src/com/sun/fortress/interpreter/unit_tests/ParserJUTest.java
@@ -15,7 +15,7 @@
     trademarks of Sun Microsystems, Inc. in the U.S. and other countries.
  ******************************************************************************/
 
-package com.sun.fortress.parser_util;
+package com.sun.fortress.interpreter.unit_tests;
 
 import java.io.File;
 import java.io.FilenameFilter;
--- ProjectFortress/src/com/sun/fortress/interpreter/unit_tests/SpecDataJUTest.java
+++ ProjectFortress/src/com/sun/fortress/interpreter/unit_tests/SpecDataJUTest.java
@@ -14,24 +14,31 @@
     Sun, Sun Microsystems, the Sun logo and Java are trademarks or registered
     trademarks of Sun Microsystems, Inc. in the U.S. and other countries.
  ******************************************************************************/
+package com.sun.fortress.interpreter.unit_tests;
 
-package com.sun.fortress.interpreter.drivers;
 import java.io.IOException;
 
 import junit.framework.Test;
 import junit.framework.TestSuite;
+import com.sun.fortress.interpreter.drivers.ProjectProperties;
 
-public class NotPassingYet {
-
-    public static void main(String[] args) {
-        junit.swingui.TestRunner.run(NotPassingYet.class);
+public class SpecDataJUTest {
+    public static void main(String[] args) throws IOException {
+        junit.textui.TestRunner.run(suite());
     }
 
     public static Test suite() throws IOException {
-        String testDir = ProjectProperties.BASEDIR + "not_passing_yet";
-        TestSuite suite = new TestSuite("Test all .fss files in 'tests'.");
+        String testDir1 = ProjectProperties.FORTRESS_AUTOHOME + "/SpecData/examples/basic";
+        String testDir2 = ProjectProperties.FORTRESS_AUTOHOME +
+            "/SpecData/examples/preliminaries";
+        String testDir3 = ProjectProperties.FORTRESS_AUTOHOME +
+            "/SpecData/examples/advanced";
+        boolean failsOnly = ! ("1".equals(ProjectProperties.get("FORTRESS_JUNIT_VERBOSE")));
+        TestSuite suite = new TestSuite("Test all .fss files in 'SpecData/examples'.");
         //$JUnit-BEGIN$
-        suite.addTest(FileTests.suite(testDir, true, true));
+        suite.addTest(FileTests.suite(testDir1, failsOnly, false));
+        suite.addTest(FileTests.suite(testDir2, failsOnly, false));
+        suite.addTest(FileTests.suite(testDir3, failsOnly, false));
         //$JUnit-END$
         return suite;
     }
