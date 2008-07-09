718fa60a0781e55e2b68e87b6e09f2278e37d559
sukyoungryu <sukyoungryu@localhost>
2008-07-09 15:18:25 +0000

    [library] Implemented RR32 with Guy.  Added a good set of test data for both RR64 and RR32.

---
+++ ProjectFortress/src/com/sun/fortress/interpreter/evaluator/values/FRR32.java
@@ -0,0 +1,65 @@
+/*******************************************************************************
+    Copyright 2008 Sun Microsystems, Inc.,
+    4150 Network Circle, Santa Clara, California 95054, U.S.A.
+    All rights reserved.
+
+    U.S. Government Rights - Commercial software.
+    Government users are subject to the Sun Microsystems, Inc. standard
+    license agreement and applicable provisions of the FAR and its supplements.
+
+    Use is subject to license terms.
+
+    This distribution may include materials developed by third parties.
+
+    Sun, Sun Microsystems, the Sun logo and Java are trademarks or registered
+    trademarks of Sun Microsystems, Inc. in the U.S. and other countries.
+ ******************************************************************************/
+
+package com.sun.fortress.interpreter.evaluator.values;
+
+import com.sun.fortress.interpreter.glue.prim.RR32;
+
+public class FRR32 extends NativeConstructor.FNativeObject {
+    private final float val;
+    private static volatile NativeConstructor con;
+
+    private FRR32(float x) {
+        super(null);
+        val = x;
+    }
+
+    public double getFloat() {return val;}
+    public float getRR32() {return val;}
+    public String getString() {return Float.toString(val);}
+    public String toString() {
+        return (val+":RR32");
+    }
+    static public final FRR32 ZERO = new FRR32((float)0.0);
+    static public FRR32 make(float x) {
+        return new FRR32(x);
+    }
+    public boolean seqv(FValue v) {
+        if (!(v instanceof NativeConstructor.FNativeObject)) return false;
+        if (v instanceof FRR32) {
+            return (Float.floatToRawIntBits(getRR32()) ==
+                    Float.floatToRawIntBits(v.getRR32()));
+        }
+        if (v instanceof FInt   || v instanceof FIntLiteral   ||
+            v instanceof FLong) {
+            return (getFloat() == v.getFloat());
+        }
+        return false;
+    }
+
+    public static void setConstructor(NativeConstructor con) {
+        // WARNING!  In order to run the tests we must reset con for
+        // each new test, so it's not OK to ignore setConstructor
+        // attempts after the first one.
+        if (con==null) return;
+        FRR32.con = con;
+    }
+
+    public NativeConstructor getConstructor() {
+        return FRR32.con;
+    }
+}
---
+++ ProjectFortress/src/com/sun/fortress/interpreter/glue/prim/RR32.java
@@ -0,0 +1,210 @@
+/*******************************************************************************
+    Copyright 2008 Sun Microsystems, Inc.,
+    4150 Network Circle, Santa Clara, California 95054, U.S.A.
+    All rights reserved.
+
+    U.S. Government Rights - Commercial software.
+    Government users are subject to the Sun Microsystems, Inc. standard
+    license agreement and applicable provisions of the FAR and its supplements.
+
+    Use is subject to license terms.
+
+    This distribution may include materials developed by third parties.
+
+    Sun, Sun Microsystems, the Sun logo and Java are trademarks or registered
+    trademarks of Sun Microsystems, Inc. in the U.S. and other countries.
+ ******************************************************************************/
+
+package com.sun.fortress.interpreter.glue.prim;
+
+import java.util.List;
+
+import com.sun.fortress.interpreter.env.BetterEnv;
+import com.sun.fortress.interpreter.evaluator.Environment;
+import com.sun.fortress.interpreter.evaluator.types.FTypeObject;
+import com.sun.fortress.interpreter.evaluator.values.FBool;
+import com.sun.fortress.interpreter.evaluator.values.FFloat;
+import com.sun.fortress.interpreter.evaluator.values.FRR32;
+import com.sun.fortress.interpreter.evaluator.values.FInt;
+import com.sun.fortress.interpreter.evaluator.values.FLong;
+import com.sun.fortress.interpreter.evaluator.values.FObject;
+import com.sun.fortress.interpreter.evaluator.values.FValue;
+import com.sun.fortress.interpreter.evaluator.values.NativeConstructor;
+import com.sun.fortress.interpreter.glue.NativeMeth0;
+import com.sun.fortress.interpreter.glue.NativeMeth1;
+import com.sun.fortress.nodes.GenericWithParams;
+import com.sun.fortress.numerics.DirectedRounding;
+
+/**
+ * Functions from RR32.
+ */
+public class RR32 extends NativeConstructor {
+
+public RR32(Environment env, FTypeObject selfType, GenericWithParams def) {
+    super(env, selfType, def);
+}
+
+protected FNativeObject makeNativeObject(List<FValue> args,
+                                         NativeConstructor con) {
+    FRR32.setConstructor(this);
+    return FRR32.ZERO;
+}
+
+static private abstract class F2B extends NativeMeth0 {
+    protected abstract boolean f(float x);
+    protected final FValue act(FObject x) {
+        return FBool.make(f(x.getRR32()));
+    }
+}
+static private abstract class F2F extends NativeMeth0 {
+    protected abstract float f(float x);
+    protected final FValue act(FObject x) {
+        return FRR32.make(f(x.getRR32()));
+    }
+}
+static private abstract class F2L extends NativeMeth0 {
+    protected abstract long f(float x);
+    protected final FValue act(FObject x) {
+        return FLong.make(f(x.getRR32()));
+    }
+}
+static private abstract class F2I extends NativeMeth0 {
+    protected abstract int f(float x);
+    protected final FValue act(FObject x) {
+        return FInt.make(f(x.getRR32()));
+    }
+}
+static private abstract class FF2B extends NativeMeth1 {
+    protected abstract boolean f(float x, float y);
+    protected final FValue act(FObject x, FValue y) {
+        return FBool.make(f(x.getRR32(),y.getRR32()));
+    }
+}
+static private abstract class FF2F extends NativeMeth1 {
+    protected abstract float f(float x, float y);
+    protected final FValue act(FObject x, FValue y) {
+        return FRR32.make(f(x.getRR32(),y.getRR32()));
+    }
+}
+
+public static final class Negate extends F2F {
+    protected float f(float x) { return -x; }
+}
+public static final class Add extends FF2F {
+    protected float f(float x, float y) { return x + y; }
+}
+public static final class Sub extends FF2F {
+    protected float f(float x, float y) { return x - y; }
+}
+public static final class Mul extends FF2F {
+    protected float f(float x, float y) { return x * y; }
+}
+public static final class Div extends FF2F {
+    protected float f(float x, float y) { return x / y; }
+}
+public static final class Sqrt extends F2F {
+    protected float f(float x) { return (float)Math.sqrt(x); }
+}
+public static final class AddUp extends FF2F {
+    protected float f(float x, float y) { return DirectedRounding.addUp(x,y); }
+}
+public static final class SubUp extends FF2F {
+    protected float f(float x, float y) { return DirectedRounding.subtractUp(x,y); }
+}
+public static final class MulUp extends FF2F {
+    protected float f(float x, float y) { return DirectedRounding.multiplyUp(x,y); }
+}
+public static final class DivUp extends FF2F {
+    protected float f(float x, float y) { return DirectedRounding.divideUp(x,y); }
+}
+public static final class SqrtUp extends F2F {
+    protected float f(float x) { return DirectedRounding.sqrtUp(x); }
+}
+public static final class AddDown extends FF2F {
+    protected float f(float x, float y) { return DirectedRounding.addDown(x,y); }
+}
+public static final class SubDown extends FF2F {
+    protected float f(float x, float y) { return DirectedRounding.subtractDown(x,y);}
+}
+public static final class MulDown extends FF2F {
+    protected float f(float x, float y) { return DirectedRounding.multiplyDown(x,y);}
+}
+public static final class DivDown extends FF2F {
+    protected float f(float x, float y) { return DirectedRounding.divideDown(x,y); }
+}
+public static final class SqrtDown extends F2F {
+    protected float f(float x) { return DirectedRounding.sqrtDown(x); }
+}
+public static final class Eq extends FF2B {
+    protected boolean f(float x, float y) { return x==y; }
+}
+public static final class NEq extends FF2B {
+    protected boolean f(float x, float y) { return x!=y; }
+}
+public static final class Less extends FF2B {
+    protected boolean f(float x, float y) { return x<y; }
+}
+public static final class LessEq extends FF2B {
+    protected boolean f(float x, float y) { return x<=y; }
+}
+public static final class Greater extends FF2B {
+    protected boolean f(float x, float y) { return x>y; }
+}
+public static final class GreaterEq extends FF2B {
+    protected boolean f(float x, float y) { return x>=y; }
+}
+public static final class Min extends FF2F {
+    protected float f(float x, float y) { return Math.min(x,y); }
+}
+public static final class Max extends FF2F {
+    protected float f(float x, float y) { return Math.max(x,y); }
+}
+public static final class Pow extends FF2F {
+    protected float f(float x, float y) {
+        return (float)Math.pow(x,y);
+    }
+}
+public static final class Ceiling extends F2F {
+    protected float f(float x) { return (float)Math.ceil(x); }
+}
+public static final class Floor extends F2F {
+    protected float f(float x) { return (float)Math.floor(x); }
+}
+public static final class ICeiling extends F2L {
+    protected long f(float x) { return (long)Math.ceil(x); }
+}
+public static final class IFloor extends F2L {
+    protected long f(float x) { return (long)Math.floor(x); }
+}
+public static final class Truncate extends F2L {
+    protected long f(float x) { return (long)x; }
+}
+public static final class Abs extends F2F {
+    protected float f(float x) { return Math.abs(x); }
+}
+public static final class NextUp extends F2F {
+    protected float f(float x) { return DirectedRounding.nextUp(x); }
+}
+public static final class NextDown extends F2F {
+    protected float f(float x) { return DirectedRounding.nextDown(x); }
+}
+public static final class RawBits extends F2I {
+    protected int f(float x) { return java.lang.Float.floatToRawIntBits(x); }
+}
+public static final class FromRawBits extends Util.I2F {
+    protected float f(int x) { return java.lang.Float.intBitsToFloat(x); }
+}
+
+public static final class Random extends Util.F2F {
+    protected float f(float scale) { return (float)(scale*Math.random()); }
+}
+
+public static final class isInfinite extends F2B {
+    protected boolean f(float x) { return java.lang.Float.isInfinite(x); }
+}
+
+public static final class isNaN extends F2B {
+    protected boolean f(float x) { return java.lang.Float.isNaN(x); }
+}
+
+}
