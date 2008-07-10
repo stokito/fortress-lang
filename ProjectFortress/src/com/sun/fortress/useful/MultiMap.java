91ecaa7eee919dcea51d4c950be3029df8c4aa8f
sukyoungryu <sukyoungryu@localhost>
2008-07-10 10:31:27 +0000

    [copyright] Fixed copyright notices.

--- ProjectFortress/src/com/sun/fortress/useful/MultiMap.java
+++ ProjectFortress/src/com/sun/fortress/useful/MultiMap.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
-    Copyright 2007 Sun Microsystems, Inc.,
+    Copyright 2008 Sun Microsystems, Inc.,
     4150 Network Circle, Santa Clara, California 95054, U.S.A.
     All rights reserved.
 
@@ -37,8 +37,8 @@ public class MultiMap<K, V> extends HashMap<K, Set<V>> implements Map<K, Set<V>>
         super();
     }
 
-    
-    
+
+
     public MultiMap(Map<? extends K, ? extends Set<V>> m) {
 		super(m);
 	}
