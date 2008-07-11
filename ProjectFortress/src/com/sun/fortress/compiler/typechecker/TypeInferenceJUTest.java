81d282214cceb56f32f7110ee4bcdee472b723cc
jrhil47 <jrhil47@localhost>
2008-07-11 22:12:31 +0000

    [TypeChecker] Added support for disjunctive formulas

--- ProjectFortress/src/com/sun/fortress/compiler/typechecker/TypeInferenceJUTest.java
+++ ProjectFortress/src/com/sun/fortress/compiler/typechecker/TypeInferenceJUTest.java
@@ -40,11 +40,18 @@ public class TypeInferenceJUTest extends TestCase {
     	TypeAnalyzer t = makeAnalyzer(trait("Int"));
     	_InferenceVarType i1=NodeFactory.make_InferenceVarType();
     	_InferenceVarType i2=NodeFactory.make_InferenceVarType();
+    	_InferenceVarType i3=NodeFactory.make_InferenceVarType();
     	ConstraintFormula t1 = upperBound(i1,i2,t.new SubtypeHistory()).and(
     			lowerBound(i1,type("Int"),t.new SubtypeHistory()),t.new SubtypeHistory()).and(
     					upperBound(i2,type("Int"),t.new SubtypeHistory()), t.new SubtypeHistory()).and(
     							upperBound(i2,i1,t.new SubtypeHistory()),t.new SubtypeHistory());
     	assertTrue(t1.isSatisfiable());
+    	t = makeAnalyzer(trait("A"),trait("B"),trait("C","B"));
+    	ConstraintFormula t2 =  upperBound(i1,type("A"),t.new SubtypeHistory()).and(lowerBound(i1,type("A"),t.new SubtypeHistory()),t.new SubtypeHistory());
+    	ConstraintFormula t3 =  t2.and(upperBound(i2,type("B"),t.new SubtypeHistory()).and(lowerBound(i2,type("B"),t.new SubtypeHistory()),t.new SubtypeHistory()),t.new SubtypeHistory());
+    	ConstraintFormula t4 =  t3.and(upperBound(i3,type("C"),t.new SubtypeHistory()).and(lowerBound(i3,type("C"),t.new SubtypeHistory()),t.new SubtypeHistory()),t.new SubtypeHistory());
+    	ConstraintFormula t5 = t4.and(lowerBound(i2,NodeFactory.makeIntersectionType(i1, i3),t.new SubtypeHistory()),t.new SubtypeHistory());
+    	assertTrue(t5.isSatisfiable());
     	} finally { debug.logEnd(); }
     }
 }
