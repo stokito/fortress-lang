51314d48f985357b97e54d615cd9e8e5aef6c5df
sukyoungryu <sukyoungryu@localhost>
2008-07-24 10:09:37 +0000

    [copyright] Fixed copyright notices.

--- ProjectFortress/src/com/sun/fortress/compiler/environments/EnvSymbolNames.java
+++ ProjectFortress/src/com/sun/fortress/compiler/environments/EnvSymbolNames.java
@@ -1,3 +1,20 @@
+/*******************************************************************************
+  Copyright 2008 Sun Microsystems, Inc.,
+  4150 Network Circle, Santa Clara, California 95054, U.S.A.
+  All rights reserved.
+
+  U.S. Government Rights - Commercial software.
+  Government users are subject to the Sun Microsystems, Inc. standard
+  license agreement and applicable provisions of the FAR and its supplements.
+
+  Use is subject to license terms.
+
+  This distribution may include materials developed by third parties.
+
+  Sun, Sun Microsystems, the Sun logo and Java are trademarks or registered
+  trademarks of Sun Microsystems, Inc. in the U.S. and other countries.
+  ******************************************************************************/
+
 package com.sun.fortress.compiler.environments;
 
 import java.util.HashMap;
@@ -12,16 +29,16 @@ import edu.rice.cs.plt.collect.Relation;
  * This data structure maps a Fortress namespace onto a set of top-level declarations.
  */
 public class EnvSymbolNames {
-	
+
 	private Map<EnvironmentClass, Set<String>> _names;
-	
+
 	public EnvSymbolNames() {
 		_names = new HashMap<EnvironmentClass, Set<String>>();
 		for(EnvironmentClass eClass : EnvironmentClass.values()) {
 			_names.put(eClass, new HashSet<String>());
 		}
 	}
-	
+
 	public void add(EnvironmentClass eClass, String name) {
 		Set<String> eClassNames = _names.get(eClass);
 		eClassNames.add(name);
@@ -30,12 +47,12 @@ public class EnvSymbolNames {
 	public Set<String> getSymbolNames(EnvironmentClass eClass) {
 		return _names.get(eClass);
 	}
-	
+
 	public Relation<String, Integer> makeHashCodeRelation(EnvironmentClass eClass) {
 		Set<String> eClassNames = _names.get(eClass);
 		Relation<String, Integer> retval = new IndexedRelation<String,Integer>();
 		for(String name : eClassNames) {
-			retval.add(name, new Integer(name.hashCode()));			
+			retval.add(name, new Integer(name.hashCode()));
 		}
 		return retval;
 	}
