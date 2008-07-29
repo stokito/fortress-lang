9cb85892a4cb8b7bd93baf1736d2ae4a23484718
jon <jon@localhost>
2008-07-29 02:39:42 +0000

    [tests] add a test for the pretty printer

---
+++ ProjectFortress/src/com/sun/fortress/tools/AstJUTest.java
@@ -0,0 +1,83 @@
+/*******************************************************************************
+    Copyright 2008 Sun Microsystems, Inc.,
+    4150 Network Circle, Santa Clara, California 95054, U.S.A.
+    All rights reserved.
+
+    U.S. Government Rights - Commercial software.
+    Government users are subject to the Sun Microsystems, Inc. standard
+    license agreement and applicable provisions of the FAR and its supplements.
+
+    Use is subject to license terms.
+
+    This distribution may include materials developed by third parties.
+
+    Sun, Sun Microsystems, the Sun logo and Java are trademarks or registered
+    trademarks of Sun Microsystems, Inc. in the U.S. and other countries.
+ ******************************************************************************/
+
+package com.sun.fortress.tools;
+
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+import junit.framework.Test;
+
+import java.util.Arrays;
+
+import java.io.FilenameFilter;
+import java.io.FileNotFoundException;
+import java.io.File;
+import java.io.IOException;
+
+import com.sun.fortress.nodes.Node;
+
+import com.sun.fortress.nodes_util.NodeFactory;
+
+import com.sun.fortress.compiler.Parser;
+
+import com.sun.fortress.repository.ProjectProperties;
+
+public class AstJUTest extends TestCase {
+
+    private static final char SEP = File.separatorChar;
+
+    private String file;
+
+    public AstJUTest( String file ){
+        super(file);
+        this.file = file;
+    }
+
+    @Override public void runTest() throws FileNotFoundException, IOException {
+        assertEquals(parse(new File(file)), parse(unparse(parse(new File(file)))));
+    }
+
+    private Node parse(String buffer) throws IOException {
+        return Parser.parseString(NodeFactory.makeAPIName(file), buffer);
+    }
+
+    private Node parse(File file) throws FileNotFoundException, IOException {
+        return Parser.parseFile(NodeFactory.makeAPIName(file.getName()), file);
+    }
+
+    private String unparse(Node node){
+        return node.accept( new FortressAstToConcrete() );
+    }
+
+    private static Iterable<String> allTests( String dir ){
+        return Arrays.asList(new File(dir).list( new FilenameFilter(){
+            public boolean accept( File dir, String name ){
+                return name.endsWith( ".fss" ) ||
+                       name.endsWith( ".fsi" );
+            }
+        }));
+    }
+
+    public static Test suite() throws IOException {
+       String tests = ProjectProperties.FORTRESS_AUTOHOME + "/ProjectFortress/tests";
+       TestSuite suite = new TestSuite("Parses all .fss and .fsi files in ProjectFortress/tests" );
+       for ( String file : allTests(tests) ){
+           suite.addTest( new AstJUTest(tests + SEP + file) );
+       }
+       return suite;
+    }
+}
