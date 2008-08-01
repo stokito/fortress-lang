42bb70c123072c51f1ca662186d38fa9d34e4b89
sukyoungryu <sukyoungryu@localhost>
2008-08-01 15:46:35 +0000

    [tool] 'fortress api -out blah.fsi blah.fss' automatically generates an API from a given component.

--- ProjectFortress/src/com/sun/fortress/nodes_util/ApiMaker.java
+++ ProjectFortress/src/com/sun/fortress/nodes_util/ApiMaker.java
@@ -44,137 +44,53 @@ public final class ApiMaker extends NodeUpdateVisitor {
         return result;
     }
 
-    public Node forComponent(Component that) {
-        APIName name_result = (APIName) that.getName().accept(this);
-        List<Import> imports_result = recurOnListOfImport(that.getImports());
-        List<Export> exports_result = recurOnListOfExport(that.getExports());
-        List<AbsDecl> decls_result = declsToAbsDecls(that.getDecls());
-        return new Api(that.getSpan(), name_result, imports_result, decls_result);
+    public Api forComponent(Component that) {
+        return new Api(that.getSpan(),
+                       that.getName(),
+                       that.getImports(),
+                       declsToAbsDecls(that.getDecls()));
     }
 
-    public Node forTraitDecl(TraitDecl that) {
-        List<Modifier> mods_result = recurOnListOfModifier(that.getMods());
-        Id name_result = (Id) that.getName().accept(this);
-        List<StaticParam> staticParams_result = recurOnListOfStaticParam(that.getStaticParams());
-        List<TraitTypeWhere> extendsClause_result = recurOnListOfTraitTypeWhere(that.getExtendsClause());
-        WhereClause where_result = (WhereClause) that.getWhere().accept(this);
-        List<BaseType> excludes_result = recurOnListOfBaseType(that.getExcludes());
-        Option<List<BaseType>> comprises_result = recurOnOptionOfListOfBaseType(that.getComprises());
-        List<AbsDecl> decls_result = declsToAbsDecls(that.getDecls());
-
+    public AbsTraitDecl forTraitDecl(TraitDecl that) {
         return new AbsTraitDecl(that.getSpan(),
-                                mods_result,
-                                name_result,
-                                staticParams_result,
-                                extendsClause_result,
-                                where_result,
-                                excludes_result,
-                                comprises_result,
-                                decls_result);
+                                that.getMods(),
+                                that.getName(),
+                                that.getStaticParams(),
+                                that.getExtendsClause(),
+                                that.getWhere(),
+                                that.getExcludes(),
+                                that.getComprises(),
+                                declsToAbsDecls(that.getDecls()));
     }
 
-    public Node forObjectDecl(ObjectDecl that) {
-        List<Modifier> mods_result = recurOnListOfModifier(that.getMods());
-        Id name_result = (Id) that.getName().accept(this);
-        List<StaticParam> staticParams_result = recurOnListOfStaticParam(that.getStaticParams());
-        List<TraitTypeWhere> extendsClause_result = recurOnListOfTraitTypeWhere(that.getExtendsClause());
-        WhereClause where_result = (WhereClause) that.getWhere().accept(this);
-        Option<List<Param>> params_result = recurOnOptionOfListOfParam(that.getParams());
-        Option<List<BaseType>> throwsClause_result = recurOnOptionOfListOfBaseType(that.getThrowsClause());
-        Contract contract_result = (Contract) that.getContract().accept(this);
-        List<AbsDecl> decls_result = declsToAbsDecls(that.getDecls());
-
-                return new AbsObjectDecl(that.getSpan(),
-                                 mods_result,
-                                 name_result,
-                                 staticParams_result,
-                                 extendsClause_result,
-                                 where_result,
-                                 params_result,
-                                 throwsClause_result,
-                                 contract_result,
-                                 decls_result);
+    public AbsObjectDecl forObjectDecl(ObjectDecl that) {
+        return new AbsObjectDecl(that.getSpan(),
+                                 that.getMods(),
+                                 that.getName(),
+                                 that.getStaticParams(),
+                                 that.getExtendsClause(),
+                                 that.getWhere(),
+                                 that.getParams(),
+                                 that.getThrowsClause(),
+                                 that.getContract(),
+                                 declsToAbsDecls(that.getDecls()));
     }
 
-    public Node forVarDeclOnly(VarDecl that, List<LValueBind> lhs_result, Expr init_result) {
-        return new AbsVarDecl(that.getSpan(), lhs_result);
+    public AbsVarDecl forVarDecl(VarDecl that) {
+        return new AbsVarDecl(that.getSpan(),
+                              that.getLhs());
     }
 
-    public Node forFnDefOnly(FnDef that,
-                             List<Modifier> mods_result,
-                             IdOrOpOrAnonymousName name_result,
-                             List<StaticParam> staticParams_result,
-                             List<Param> params_result,
-                             Option<Type> returnType_result,
-                             Option<List<BaseType>> throwsClause_result,
-                             WhereClause where_result,
-                             Contract contract_result,
-                             Expr body_result) {
+    public AbsFnDecl forFnDef(FnDef that) {
         return new AbsFnDecl(that.getSpan(),
-                             mods_result,
-                             name_result,
-                             staticParams_result,
-                             params_result,
-                             returnType_result,
-                             throwsClause_result,
-                             where_result,
-                             contract_result,
+                             that.getMods(),
+                             that.getName(),
+                             that.getStaticParams(),
+                             that.getParams(),
+                             that.getReturnType(),
+                             that.getThrowsClause(),
+                             that.getWhere(),
+                             that.getContract(),
                              that.getSelfName());
-    }
-
-    public Node forExternalSyntaxOnly(ExternalSyntax that,
-                                      IdOrOpOrAnonymousName openExpander_result,
-                                      Id name_result,
-                                      IdOrOpOrAnonymousName closeExpander_result,
-                                      Expr expr_result) {
-        return new AbsExternalSyntax(that.getSpan(),
-                                     openExpander_result,
-                                     name_result,
-                                     closeExpander_result);
-    }
-
-
-
-    // The following for- methods are overridden solely as an optimization;
-    // we can short-circuit processing of AST fragments that are simply
-    // thrown away by the above forOnly- methods.
-
-    public Node forVarDecl(VarDecl that) {
-        List<LValueBind> lhs_result = recurOnListOfLValueBind(that.getLhs());
-        // No need to recur on the body, since we throw it away.
-        Expr init_result = null;
-        return forVarDeclOnly(that, lhs_result, init_result);
-    }
-
-    public Node forFnDef(FnDef that) {
-        List<Modifier> mods_result = recurOnListOfModifier(that.getMods());
-        IdOrOpOrAnonymousName name_result = (IdOrOpOrAnonymousName) that.getName().accept(this);
-        List<StaticParam> staticParams_result = recurOnListOfStaticParam(that.getStaticParams());
-        List<Param> params_result = recurOnListOfParam(that.getParams());
-        Option<Type> returnType_result = recurOnOptionOfType(that.getReturnType());
-        Option<List<BaseType>> throwsClause_result = recurOnOptionOfListOfBaseType(that.getThrowsClause());
-        WhereClause where_result = (WhereClause) that.getWhere().accept(this);
-        Contract contract_result = (Contract) that.getContract().accept(this);
-        // No need to recur on the body, since we throw it away.
-        Expr body_result = null;
-        return forFnDefOnly(that,
-                            mods_result,
-                            name_result,
-                            staticParams_result,
-                            params_result,
-                            returnType_result,
-                            throwsClause_result,
-                            where_result,
-                            contract_result,
-                            body_result);
-    }
-
-    public Node forExternalSyntax(ExternalSyntax that) {
-        IdOrOpOrAnonymousName openExpander_result = (IdOrOpOrAnonymousName) that.getOpenExpander().accept(this);
-        Id name_result = (Id) that.getName().accept(this);
-        IdOrOpOrAnonymousName closeExpander_result = (IdOrOpOrAnonymousName) that.getCloseExpander().accept(this);
-        // No need to recur on the body, since we throw it away.
-        Expr expr_result = null;
-        return forExternalSyntaxOnly(that, openExpander_result, name_result, closeExpander_result, expr_result);
     }
 }
