1d590dc8af9d87e70c5fd1f7c8813f925bfba867
sukyoungryu <sukyoungryu@localhost>
2008-08-02 11:53:55 +0000

    [copyright] Fixed copyright notices.

--- ProjectFortress/src/com/sun/fortress/compiler/OverloadRewriteVisitor.java
+++ ProjectFortress/src/com/sun/fortress/compiler/OverloadRewriteVisitor.java
@@ -1,3 +1,20 @@
+/*******************************************************************************
+    Copyright 2008 Sun Microsystems, Inc.,
+    4150 Network Circle, Santa Clara, California 95054, U.S.A.
+    All rights reserved.
+
+    U.S. Government Rights - Commercial software.
+    Government users are subject to the Sun Microsystems, Inc. standard
+    license agreement and applicable provisions of the FAR and its supplements.
+
+    Use is subject to license terms.
+
+    This distribution may include materials developed by third parties.
+
+    Sun, Sun Microsystems, the Sun logo and Java are trademarks or registered
+    trademarks of Sun Microsystems, Inc. in the U.S. and other countries.
+ ******************************************************************************/
+
 package com.sun.fortress.compiler;
 
 import java.util.Collections;
@@ -16,9 +33,9 @@ public class OverloadRewriteVisitor extends NodeUpdateVisitor {
 
     final private Map<String, List<Id>> overloadedFunctions = new HashMap<String, List<Id>>();
     final private Map<String, List<OpName>> overloadedOperators = new HashMap<String, List<OpName>>();
-    
+
     @Override
-    public Node forFnRefOnly(FnRef that, Option<Type> exprType_result, Id originalName, List<Id> fns, 
+    public Node forFnRefOnly(FnRef that, Option<Type> exprType_result, Id originalName, List<Id> fns,
             List<StaticArg> staticArgs) {
         if (fns.size() > 1) {
             Collections.<Id>sort(fns, NodeComparator.idComparer);
@@ -41,9 +58,9 @@ public class OverloadRewriteVisitor extends NodeUpdateVisitor {
         }
         return super.forFnRefOnly(that, exprType_result , originalName, fns, staticArgs);
     }
-    
 
-    @Override    
+
+    @Override
     public Node forOpRefOnly(OpRef that, Option<Type> exprType_result, OpName originalName, List<OpName> ops,
             List<StaticArg> staticArgs) {
         if (ops.size() > 1) {
@@ -68,8 +85,8 @@ public class OverloadRewriteVisitor extends NodeUpdateVisitor {
         return super.forOpRefOnly(that, exprType_result, originalName, ops, staticArgs);
     }
 
-    
-    public Map<String, List<Id>> getOverloadedFunctions() { 
+
+    public Map<String, List<Id>> getOverloadedFunctions() {
         return overloadedFunctions;
     }
 
@@ -77,5 +94,5 @@ public class OverloadRewriteVisitor extends NodeUpdateVisitor {
         return overloadedOperators;
     }
 
-    
+
 }
--- ProjectFortress/src/com/sun/fortress/compiler/phases/OverloadRewritingPhase.java
+++ ProjectFortress/src/com/sun/fortress/compiler/phases/OverloadRewritingPhase.java
@@ -1,3 +1,20 @@
+/*******************************************************************************
+    Copyright 2008 Sun Microsystems, Inc.,
+    4150 Network Circle, Santa Clara, California 95054, U.S.A.
+    All rights reserved.
+
+    U.S. Government Rights - Commercial software.
+    Government users are subject to the Sun Microsystems, Inc. standard
+    license agreement and applicable provisions of the FAR and its supplements.
+
+    Use is subject to license terms.
+
+    This distribution may include materials developed by third parties.
+
+    Sun, Sun Microsystems, the Sun logo and Java are trademarks or registered
+    trademarks of Sun Microsystems, Inc. in the U.S. and other countries.
+ ******************************************************************************/
+
 package com.sun.fortress.compiler.phases;
 
 import com.sun.fortress.compiler.AnalyzeResult;
@@ -19,7 +36,7 @@ public class OverloadRewritingPhase extends Phase {
         Debug.debug(Debug.Type.FORTRESS, 1, "Start phase Overload Rewriting");
         AnalyzeResult previous = parentPhase.getResult();
 
-        OverloadRewriter.ComponentResult results = 
+        OverloadRewriter.ComponentResult results =
             OverloadRewriter.rewriteComponents(previous.components(), env);
 
         if (!results.isSuccessful()) {
