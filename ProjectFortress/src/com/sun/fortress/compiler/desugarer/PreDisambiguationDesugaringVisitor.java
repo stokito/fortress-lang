fbae65c8f9229179e4611287904bc3eefd841212
sukyoungryu <sukyoungryu@localhost>
2008-08-06 15:04:15 +0000

    [desugarer] 1. Moved some utilities for desugarers to nodes_util/DesugarerUtil.java 2. Merged two pre-disambiguation desugarers (ConditionalOpDesugarer and ExtendsObjectVisitor) to PreDisambiguationDesugaringVisitor 3. Added comments about each compiler phase.

---
+++ ProjectFortress/src/com/sun/fortress/compiler/desugarer/PreDisambiguationDesugaringVisitor.java
@@ -0,0 +1,210 @@
+/*******************************************************************************
+ Copyright 2008 Sun Microsystems, Inc.,
+ 4150 Network Circle, Santa Clara, California 95054, U.S.A.
+ All rights reserved.
+
+ U.S. Government Rights - Commercial software.
+ Government users are subject to the Sun Microsystems, Inc. standard
+ license agreement and applicable provisions of the FAR and its supplements.
+
+ Use is subject to license terms.
+
+ This distribution may include materials developed by third parties.
+
+ Sun, Sun Microsystems, the Sun logo and Java are trademarks or registered
+ trademarks of Sun Microsystems, Inc. in the U.S. and other countries.
+ ******************************************************************************/
+
+package com.sun.fortress.compiler.desugarer;
+
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+import edu.rice.cs.plt.iter.IterUtil;
+import edu.rice.cs.plt.tuple.Option;
+
+import com.sun.fortress.nodes.*;
+import com.sun.fortress.nodes_util.DesugarerUtil;
+import com.sun.fortress.nodes_util.ExprFactory;
+import com.sun.fortress.nodes_util.NodeFactory;
+import com.sun.fortress.nodes_util.OprUtil;
+import com.sun.fortress.nodes_util.Span;
+import com.sun.fortress.interpreter.glue.WellKnownNames;
+import com.sun.fortress.useful.Useful;
+
+import static com.sun.fortress.nodes_util.DesugarerUtil.*;
+import static com.sun.fortress.exceptions.InterpreterBug.bug;
+
+/** Run desugaring phases that must occur before disambiguation.
+ *  1) Remove conditional operators, replacing their operands with thunks.
+ *  Desugar conditional operators into operators that take thunks.
+ *  This desugaring is described in section 22.8 of the specification.
+ *  We find {@code e_1 AND: e_2}, for example, and change it into
+ *  {@code e_1 AND (fn () => e_2)}, for which an overloading must exist.
+ *  This desugaring must go before disambiguation, and is therefore called
+ *  by {@code PreDisambiguationDesugarer}.
+ *  2) Rewrite trait, object, and object expressions to explicitly extend Object.
+ */
+public class PreDisambiguationDesugaringVisitor extends NodeUpdateVisitor {
+
+    private final Id anyTypeId = new Id(WellKnownNames.anyTypeName);
+
+    /** If the extends clause of a trait declaration, an object declaration, or
+     *  an object expression is empty, then replace the empty extends clause
+     *  with {Object}.
+     */
+    private List<TraitTypeWhere> rewriteExtendsClause(Node whence,
+                                                      List<TraitTypeWhere> extendsClause) {
+        if (extendsClause.size() > 0) return extendsClause;
+        Id objectId = NodeFactory.makeId(whence.getSpan(),
+                                         WellKnownNames.objectTypeName);
+        TraitType typeObject = NodeFactory.makeTraitType(objectId);
+        TraitTypeWhere extendsObject = NodeFactory.makeTraitTypeWhere(typeObject);
+        return Collections.singletonList(extendsObject);
+    }
+
+    @Override
+        public Node forObjectExprOnly(ObjectExpr that,
+                                      Option<Type> exprType_result,
+                                      List<TraitTypeWhere> extendsClause,
+                                      List<Decl> decls) {
+        extendsClause = rewriteExtendsClause(that, extendsClause);
+        return super.forObjectExprOnly(that, exprType_result, extendsClause, decls);
+    }
+
+    @Override
+        public Node forAbsTraitDeclOnly(AbsTraitDecl that,
+                                        List<Modifier> mods,
+                                        Id name,
+                                        List<StaticParam> staticParams,
+                                        List<TraitTypeWhere> extendsClause,
+                                        WhereClause where,
+                                        List<BaseType> excludes,
+                                        Option<List<BaseType>> comprises,
+                                        List<AbsDecl> decls) {
+        if (!that.getName().equals(anyTypeId)) {
+            extendsClause = rewriteExtendsClause(that, extendsClause);
+        }
+        return super.forAbsTraitDeclOnly(that, mods, name, staticParams, extendsClause,
+                                         where, excludes, comprises, decls);
+    }
+
+    @Override
+        public Node forTraitDeclOnly(TraitDecl that,
+                                     List<Modifier> mods,
+                                     Id name,
+                                     List<StaticParam> staticParams,
+                                     List<TraitTypeWhere> extendsClause,
+                                     WhereClause where,
+                                     List<BaseType> excludes,
+                                     Option<List<BaseType>> comprises,
+                                     List<Decl> decls) {
+        if (!that.getName().equals(anyTypeId)) {
+            extendsClause = rewriteExtendsClause(that, extendsClause);
+        }
+        return super.forTraitDeclOnly(that, mods, name, staticParams, extendsClause,
+                                      where, excludes, comprises, decls);
+    }
+
+    @Override
+        public Node forAbsObjectDeclOnly(AbsObjectDecl that,
+                                         List<Modifier> mods,
+                                         Id name,
+                                         List<StaticParam> staticParams,
+                                         List<TraitTypeWhere> extendsClause,
+                                         WhereClause where,
+                                         Option<List<Param>> params,
+                                         Option<List<BaseType>> throwsClause,
+                                         Contract contract,
+                                         List<AbsDecl> decls) {
+        extendsClause = rewriteExtendsClause(that, extendsClause);
+        return super.forAbsObjectDeclOnly(that, mods, name, staticParams, extendsClause,
+                                          where, params, throwsClause, contract, decls);
+    }
+
+    @Override
+        public Node forObjectDeclOnly(ObjectDecl that,
+                                      List<Modifier> mods,
+                                      Id name,
+                                      List<StaticParam> staticParams,
+                                      List<TraitTypeWhere> extendsClause,
+                                      WhereClause where,
+                                      Option<List<Param>> params,
+                                      Option<List<BaseType>> throwsClause,
+                                      Contract contract,
+                                      List<Decl> decls) {
+        extendsClause = rewriteExtendsClause(that, extendsClause);
+        return super.forObjectDeclOnly(that, mods, name, staticParams, extendsClause,
+                                       where, params, throwsClause, contract, decls);
+    }
+
+    @Override
+	public Node forAmbiguousMultifixOpExpr(AmbiguousMultifixOpExpr that) {
+        // If there is a colon at all, the operator is no longer ambiguous:
+        // It must be infix.
+        OpName op_name = IterUtil.first(that.getInfix_op().getOps());
+        boolean prefix = OprUtil.hasPrefixColon(op_name);
+        boolean suffix = OprUtil.hasSuffixColon(op_name);
+
+        if( prefix || suffix ) {
+            OpExpr new_op = new OpExpr(that.getSpan(), that.isParenthesized(), that.getInfix_op(), that.getArgs());
+            return recur(new_op);
+        }
+        else {
+            return super.forAmbiguousMultifixOpExpr(that);
+        }
+    }
+
+    @Override
+	public Node forOpExpr(OpExpr that) {
+        OpRef op_result = (OpRef) recur(that.getOp());
+        List<Expr> args_result = recurOnListOfExpr(that.getArgs());
+
+        OpExpr new_op;
+        if( op_result == that.getOp() && args_result == that.getArgs() ) {
+            new_op = that;
+        }
+        else {
+            new_op = new OpExpr(that.getSpan(), that.isParenthesized(), op_result, args_result);
+        }
+        return cleanupOpExpr(new_op);
+    }
+
+    private static Expr thunk(Expr e) {
+        return ExprFactory.makeFnExpr(e.getSpan(),
+                                      Collections.<Param>emptyList(), e);
+    }
+
+    private Expr cleanupOpExpr(OpExpr opExp) {
+        OpRef ref = opExp.getOp();
+
+        List<Expr> args = opExp.getArgs();
+
+        if (args.size() <= 1) return opExp;
+        OpName qop = IterUtil.first(ref.getOps());
+
+        if (OprUtil.isEnclosing(qop)) return opExp;
+        if (OprUtil.isUnknownFixity(qop))
+            return bug(opExp, "The operator fixity is unknown: " +
+                       ((Op)qop).getText());
+        boolean prefix = OprUtil.hasPrefixColon(qop);
+        boolean suffix = OprUtil.hasSuffixColon(qop);
+        if (!prefix && !suffix) return opExp;
+        qop = OprUtil.noColon(qop);
+        Iterator<Expr> i = args.iterator();
+        Expr res = i.next();
+        Span sp = opExp.getSpan();
+        while (i.hasNext()) {
+            Expr arg = (Expr)i.next();
+            if (prefix) {
+                res = thunk(res);
+            }
+            if (suffix) {
+                arg = thunk(arg);
+            }
+            res = ExprFactory.makeOpExpr(sp,qop,res,arg);
+        }
+        return res;
+    }
+
+}
---
+++ ProjectFortress/src/com/sun/fortress/nodes_util/DesugarerUtil.java
@@ -0,0 +1,247 @@
+/*******************************************************************************
+    Copyright 2008 Sun Microsystems, Inc.,
+    4150 Network Circle, Santa Clara, California 95054, U.S.A.
+    All rights reserved.
+
+    U.S. Government Rights - Commercial software.
+    Government users are subject to the Sun Microsystems, Inc. standard
+    license agreement and applicable provisions of the FAR and its supplements.
+
+    Use is subject to license terms.
+
+    This distribution may include materials developed by third parties.
+
+    Sun, Sun Microsystems, the Sun logo and Java are trademarks or registered
+    trademarks of Sun Microsystems, Inc. in the U.S. and other countries.
+ ******************************************************************************/
+
+package com.sun.fortress.nodes_util;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import com.sun.fortress.nodes.*;
+import com.sun.fortress.interpreter.glue.WellKnownNames;
+import com.sun.fortress.useful.Useful;
+
+public class DesugarerUtil {
+
+    // Distinct from Types.ANY_NAME because qualified names aren't yet supported
+    public final static Id INTERNAL_ANY_NAME =
+        NodeFactory.makeId("Any");
+
+    public final static Id LOOP_NAME =
+        NodeFactory.makeId(WellKnownNames.loop);
+
+    public final static VarRef GENERATE_NAME =
+        ExprFactory.makeVarRef(WellKnownNames.generate);
+
+    public final static VarRef MAP_NAME =
+        ExprFactory.makeVarRef(WellKnownNames.map);
+
+    public final static VarRef SINGLETON_NAME =
+        ExprFactory.makeVarRef(WellKnownNames.singleton);
+
+    public final static VarRef NEST_NAME =
+        ExprFactory.makeVarRef(WellKnownNames.nest);
+
+    public final static VarRef COND_NAME =
+        ExprFactory.makeVarRef(WellKnownNames.cond);
+
+    public final static VarRef WHILECOND_NAME =
+        ExprFactory.makeVarRef(WellKnownNames.whileCond);
+
+    public final static VarRef BIGOP_NAME =
+        ExprFactory.makeVarRef(WellKnownNames.bigOperator);
+
+    public final static VarRef BIGOP2_NAME =
+        ExprFactory.makeVarRef(WellKnownNames.bigOperator);
+
+    public final static VarRef FILTER_NAME =
+        ExprFactory.makeVarRef(WellKnownNames.filter);
+
+    /**
+     * Used to generate temporary names when rewriting (for example)
+     * tuple initializations.
+     */
+    private static int tempCount = 0;
+
+    public static String gensym(String prefix) {
+        return prefix + "$" + (++tempCount);
+    }
+
+    public static String gensym() {
+        return gensym("t");
+    }
+
+    public static Id gensymId(String prefix) {
+        return NodeFactory.makeId(gensym(prefix));
+    }
+
+    /**
+     *  Given body, binds <- exp
+     *  generate (fn binds => body)
+     */
+    public static Expr bindsAndBody(GeneratorClause g, Expr body) {
+        List<Id> binds = g.getBind();
+        List<Param> params = new ArrayList<Param>(binds.size());
+        for (Id b : binds) params.add(NodeFactory.makeParam(b));
+        Expr res = ExprFactory.makeFnExpr(g.getSpan(),params,body);
+        return res;
+    }
+
+    /** Given a list of generators, return an expression that computes
+     *  a composite generator given a reduction and a unit.  Desugars
+     *  to fn(r,u) => D where D is as follows:
+     *  exp | nothing       =>  __generate(exp,r,u)
+     *  body | x <- exp     =>  __generate(exp,r,fn x => u(body))
+     *  body | x <- exp, gs =>  __generate(exp,r,fn x => u(body)) | gs
+     */
+    public static Expr visitGenerators(Span span, List<GeneratorClause> gens, Expr body) {
+        Id reduction = gensymId("reduction");
+        VarRef redVar = ExprFactory.makeVarRef(reduction);
+        Id unitFn = gensymId("unit");
+        VarRef unitVar = ExprFactory.makeVarRef(unitFn);
+        int i = gens.size();
+        if (i==0) {
+            /* Single generator as body, with no generator clauses. */
+            body = new TightJuxt(span, false,
+                             Useful.list(GENERATE_NAME,
+                                         ExprFactory.makeTuple(body,redVar,unitVar)));
+        } else {
+            List<GeneratorClause> squozenGens =
+                new ArrayList<GeneratorClause>(gens.size());
+            GeneratorClause prevGen = gens.get(0);
+            for (int j = 1; j < i; j++) {
+                GeneratorClause gen = gens.get(j);
+                GeneratorClause squeeze = filterSqueezeOpportunity(prevGen,gen);
+                if (squeeze!=null) {
+                    // System.out.println(gen+": Filter squeeze opportunity\n"+
+                    //                    squeeze.toStringVerbose());
+                    prevGen = squeeze;
+                } else {
+                    squozenGens.add(prevGen);
+                    prevGen=gen;
+                }
+            }
+            squozenGens.add(prevGen);
+            gens = squozenGens;
+            i = gens.size();
+            if (nestedGeneratorOpportunity(gens,body)) {
+                System.out.println(span+" and\n"+body.getSpan()+
+                                   ": Generator of generator opportunity?");
+            }
+            body = new TightJuxt(body.getSpan(), false,
+                                 Useful.list(unitVar, body));
+            for (i--; i>=0; i--) {
+                body = oneGenerator(gens.get(i), redVar, body);
+            }
+        }
+        return ExprFactory.makeFnExpr(span,
+                       Useful.<Param>list(NodeFactory.makeParam(reduction),
+                                          NodeFactory.makeParam(unitFn)),
+                                      body);
+    }
+
+    /** Given a list of generator clauses and a position i, determine
+     * if the i^th generator clause is a predicate that can be fused
+     * with the first immediately preceding variable-labeled generator
+     * clause.  This requires that the comprehension be of the form:
+     * x <- gs, p(x) where p(x) is literally a function call, but where
+     * x might actually be a tuple of variables.  That way we can pass
+     * the literal function p to filter and make use of its
+     * properties.  When we have full type checking, we can simply
+     * unconditionally fuse predicates; that will be strictly better
+     * than the present approach.
+     */
+    private static GeneratorClause filterSqueezeOpportunity(GeneratorClause prevGen,
+                                                            GeneratorClause gen) {
+        // Clause is predicate
+        if (gen.getBind().size()!=0) return null;
+        // Check shape of clause expression
+        Expr predicate = gen.getInit();
+        if (!(predicate instanceof Juxt)) return null;
+        List<Expr>exprs = ((Juxt)predicate).getExprs();
+        if (exprs.size() != 2) return null;
+        Expr fn = exprs.get(0);
+        if (!(fn instanceof VarRef)) return null;
+        // Argument handling.  Handle (a,b,c) <- xs, p(a,b,c) as well.
+        Expr arg = exprs.get(1);
+        List<Expr> args;
+        if (arg instanceof VarRef) {
+            args = Collections.<Expr>singletonList(arg);
+        } else if (arg instanceof TupleExpr) {
+            args = ((TupleExpr) arg).getExprs();
+        } else {
+            return null;
+        }
+        // Find all the argument variables, and check that each matches the
+        // corresponding variable in the binding of the previous generator.
+        List<Id> binds = prevGen.getBind();
+        int i = 0;
+        if (binds.size() != args.size()) return null;
+        for (Expr a : args) {
+            if (!(a instanceof VarRef)) return null;
+            if (!(binds.get(i).equals(((VarRef)a).getVar()))) return null;
+            i++;
+        }
+        // FILTER SQUEEZE.  Want to generate a new clause of the form
+        // binds <- __filter(init,fn)
+        Expr init = prevGen.getInit();
+        Expr filtered =
+            ExprFactory.makeTightJuxt(gen.getSpan(), FILTER_NAME,
+                                      ExprFactory.makeTuple(init,fn));
+        GeneratorClause res =
+            ExprFactory.makeGeneratorClause(prevGen.getSpan(), binds, filtered);
+        return res;
+    }
+
+    /** Given outer generator clauses and inner body expression,
+     * determine if there's an opportunity for generator-of-generator.
+     * This is true if the generator expression has the form
+     *   BIG OP1[gs1, xs <- gOfg] BIG OP2[x <- xs, gs2] body
+     * In this case we should desugar into a generator-of-generators
+     * which we can naively think of as this:
+     *   BIG OP1[gs1] (BIG OP1[xs<-gOfg] BIG OP2 [x<-xs] (BIG OP2[gs2] body))
+     * where the middle two are accomplished by a single call to
+     * __generate2 with a pair of reductions.
+     */
+    private static boolean nestedGeneratorOpportunity(List<GeneratorClause> gens, Expr body) {
+        // Make sure there are outer generators.
+        int gs = gens.size();
+        if (gs==0) return false;
+        // Make sure body is an Accumulator expression.
+        if (!(body instanceof Accumulator)) return false;
+        Accumulator bodyAccum = (Accumulator)body;
+        // Make sure innermost generator of outer accumulator yields a single variable.
+        GeneratorClause outerGen = gens.get(gs-1);
+        List<Id> outerVars = outerGen.getBind();
+        if (outerVars.size()!=1) return false;
+        Id outerVar = outerVars.get(0);
+        // Find outermost generator of inner accumulator (might be body)
+        List<GeneratorClause> bodyGens = bodyAccum.getGens();
+        Expr bodyOuterInit;
+        if (bodyGens.size()==0) {
+            bodyOuterInit = bodyAccum.getBody();
+        } else {
+            GeneratorClause bodyOuterGen = bodyGens.get(0);
+            bodyOuterInit = bodyOuterGen.getInit();
+        }
+        // Make sure innermost generator is a single variable matching outerVar
+        if (!(bodyOuterInit instanceof VarRef)) return false;
+        Id innerVar = ((VarRef)bodyOuterInit).getVar();
+        return (outerVar.equals(innerVar));
+    }
+
+    /**
+     *  Given reduction of body | x <- exp, yields
+     *  __generate(x,reduction,fn x => body)
+     */
+    private static Expr oneGenerator(GeneratorClause g, VarRef reduction, Expr body) {
+        Expr loopBody = bindsAndBody(g, body);
+        Expr params = ExprFactory.makeTuple(g.getInit(), reduction, loopBody);
+        return new TightJuxt(g.getSpan(), false,
+                             Useful.list(GENERATE_NAME,params));
+    }
+}
