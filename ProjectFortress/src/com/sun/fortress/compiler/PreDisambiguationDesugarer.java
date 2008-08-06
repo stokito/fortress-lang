a4b88aedcaddd7d88e798d81e1dbf23cd421f60e
sukyoungryu <sukyoungryu@localhost>
2008-08-06 15:03:36 +0000

    [desugarer] 1. Moved some utilities for desugarers to nodes_util/DesugarerUtil.java 2. Merged two pre-disambiguation desugarers (ConditionalOpDesugarer and ExtendsObjectVisitor) to PreDisambiguationDesugaringVisitor 3. Added comments about each compiler phase.

--- ProjectFortress/src/com/sun/fortress/compiler/PreDisambiguationDesugarer.java
+++ ProjectFortress/src/com/sun/fortress/compiler/PreDisambiguationDesugarer.java
@@ -20,8 +20,7 @@ package com.sun.fortress.compiler;
 import java.util.HashSet;
 import java.util.Map;
 
-import com.sun.fortress.compiler.desugarer.ConditionalOpDesugarer;
-import com.sun.fortress.compiler.desugarer.ExtendsObjectVisitor;
+import com.sun.fortress.compiler.desugarer.PreDisambiguationDesugaringVisitor;
 import com.sun.fortress.compiler.index.ApiIndex;
 import com.sun.fortress.compiler.index.ComponentIndex;
 import com.sun.fortress.exceptions.StaticError;
@@ -36,13 +35,7 @@ import edu.rice.cs.plt.iter.IterUtil;
  */
 public class PreDisambiguationDesugarer {
 
-	/** Remove conditional operators, replacing their operands with thunks. */
-	public static final boolean conditional_op_desugar = true;
-	
-	/** Rewrite trait, object, and object expressions to explicitly extend Object. */
-    public static final boolean extends_object_desugar = true;	
-	
-	public static class ComponentResult extends StaticPhaseResult {
+    public static class ComponentResult extends StaticPhaseResult {
         private final Map<APIName, ComponentIndex> _components;
         public ComponentResult(Map<APIName, ComponentIndex> components,
                                Iterable<? extends StaticError> errors) {
@@ -50,9 +43,9 @@ public class PreDisambiguationDesugarer {
             _components = components;
         }
         public Map<APIName, ComponentIndex> components() { return _components; }
-	}
+    }
 
-	public static class ApiResult extends StaticPhaseResult {
+    public static class ApiResult extends StaticPhaseResult {
         Map<APIName, ApiIndex> _apis;
 
         public ApiResult(Map<APIName, ApiIndex> apis, Iterable<? extends StaticError> errors) {
@@ -60,10 +53,10 @@ public class PreDisambiguationDesugarer {
             _apis = apis;
         }
         public Map<APIName, ApiIndex> apis() { return _apis; }
-	}
+    }
 
-	public static PreDisambiguationDesugarer.ApiResult desugarApis(
-			Map<APIName, ApiIndex> apis, GlobalEnvironment apiEnv) {
+    public static PreDisambiguationDesugarer.ApiResult desugarApis(Map<APIName, ApiIndex> apis,
+                                                                   GlobalEnvironment apiEnv) {
         HashSet<Api> desugaredApis = new HashSet<Api>();
 
         for (ApiIndex apiIndex : apis.values()) {
@@ -74,19 +67,16 @@ public class PreDisambiguationDesugarer {
             (IndexBuilder.buildApis(desugaredApis,
                                     System.currentTimeMillis()).apis(),
              IterUtil.<StaticError>empty());
-	}
+    }
 
     public static Api desugarApi(ApiIndex apiIndex, GlobalEnvironment env) {
         Api api = (Api)apiIndex.ast();
-        if(extends_object_desugar) {
-            ExtendsObjectVisitor extendsObjectVisitor = new ExtendsObjectVisitor();
-            api = (Api) api.accept(extendsObjectVisitor);
-        }        
+        api = (Api) api.accept( new PreDisambiguationDesugaringVisitor() );
         return api;
     }
-	
-	public static ComponentResult desugarComponents(
-			Map<APIName, ComponentIndex> components, GlobalEnvironment apiEnv) {
+
+    public static ComponentResult desugarComponents(Map<APIName, ComponentIndex> components,
+                                                    GlobalEnvironment apiEnv) {
         HashSet<Component> desugaredComponents = new HashSet<Component>();
         Iterable<? extends StaticError> errors = new HashSet<StaticError>();
 
@@ -97,21 +87,13 @@ public class PreDisambiguationDesugarer {
         return new ComponentResult
             (IndexBuilder.buildComponents(desugaredComponents,
                                           System.currentTimeMillis()).
-                 components(), errors);
-	}
+             components(), errors);
+    }
 
-	public static Component desugarComponent(ComponentIndex component,
-			GlobalEnvironment env) {
-		Component comp = (Component) component.ast();
-		
-		if(conditional_op_desugar) {
-			ConditionalOpDesugarer condOpDesugarer = new ConditionalOpDesugarer();
-			comp = (Component) comp.accept(condOpDesugarer);
-		}
-		if(extends_object_desugar) {
-            ExtendsObjectVisitor extendsObjectVisitor = new ExtendsObjectVisitor();
-            comp = (Component) comp.accept(extendsObjectVisitor);		    
-		}
-		return comp;
-	}
+    public static Component desugarComponent(ComponentIndex component,
+                                             GlobalEnvironment env) {
+        Component comp = (Component) component.ast();
+        comp = (Component) comp.accept( new PreDisambiguationDesugaringVisitor() );
+        return comp;
+    }
 }
