9015ca86631136959ccb8682ff1d8e6fd9073cec
jmaessen <jmaessen@localhost>
2008-08-13 14:00:59 +0000

    [interpreter] Permit unification of overloaded functions passed as higher-order parameters.  Do not, for the moment, commit to a particular overloading.  The right way to do this at run time would be to perform overloading resolution and generic instantiation of arguments at the same time as unification---but this would require gutting and completely re-writing everything to do with function application in the interpreter, and will be made obsolete by type checking in any case.  We may still need to instantiate generic parameters of a chosen overloading, but unification *doesn't* handle uninstantiated generic functions passed as parameters in any case.

--- ProjectFortress/src/com/sun/fortress/interpreter/evaluator/types/FTypeOverloadedArrow.java
+++ ProjectFortress/src/com/sun/fortress/interpreter/evaluator/types/FTypeOverloadedArrow.java
@@ -17,11 +17,19 @@
 
 package com.sun.fortress.interpreter.evaluator.types;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
+import com.sun.fortress.useful.BoundingMap;
 import com.sun.fortress.useful.Factory1;
 import com.sun.fortress.useful.Memo1;
 import com.sun.fortress.useful.Useful;
+
+import com.sun.fortress.exceptions.UnificationError;
+import com.sun.fortress.nodes.Type;
+import com.sun.fortress.interpreter.env.BetterEnv;
+import com.sun.fortress.interpreter.evaluator.Environment;
 
 public class FTypeOverloadedArrow extends FType {
 
@@ -43,26 +51,60 @@ public class FTypeOverloadedArrow extends FType {
         return memo.make(l);
     }
 
-  List<FType> l;
+    List<FType> l;
 
-  private FTypeOverloadedArrow(List<FType> l) {
-      super("Overloaded"); // TODO should construct this lazily, better
-      this.l = l;
-  }
+    private FTypeOverloadedArrow(List<FType> l) {
+        super("Overloaded"); // TODO should construct this lazily, better
+        this.l = l;
+    }
 
-  public String toString() {
-      return Useful.listInParens(l);
-  }
+    @Override
+    public String toString() {
+        return Useful.listInParens(l);
+    }
 
-  public boolean subtypeOf(FType other) {
-      if (commonSubtypeOf(other)) return true;
-      if (other instanceof FType) {
-          FType fta = (FType)(other);
-          for (FType  i : l) {
-              if (i.subtypeOf(fta)) return true;
-          }
-      }
-      return false;
-  }
+    @Override
+    public boolean subtypeOf(FType other) {
+        if (commonSubtypeOf(other)) return true;
+        if (other instanceof FType) {
+            FType fta = (FType)(other);
+            for (FType  i : l) {
+                if (i.subtypeOf(fta)) return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    protected boolean unifyNonVar(Environment env, Set<String> tp_set,
+            BoundingMap<String, FType, TypeLatticeOps> abm, Type val) {
+        // Note that this attempts unification, but then rolls back
+        // the results.  This is because we otherwise need to choose
+        // the "best" overloading based on the available static types,
+        // and that's not always going to be possible.
+
+        // To really do this right I'm now convinced that generic
+        // function instantiation (including instantiation of generic
+        // arguments), overloading, and unification must all occur as
+        // a single monolithic pass.  It's unclear that this is worth
+        // the bother if we aren't planning to retain this code after
+        // type checking is in place.
+
+        BoundingMap<String, FType, TypeLatticeOps> savedAbm = abm.copy();
+        if (FType.DUMP_UNIFY)
+            System.out.println("\tAttempting to unify overloadings.");
+        for (FType t : l) {
+            try {
+                t.unify(env,tp_set,abm,val);
+                return true;
+            } catch (UnificationError e) {
+                if (FType.DUMP_UNIFY)
+                    System.out.println("\tOverloading "+t+" != "+val+", abm ="+abm);
+            } finally {
+                abm.assign(savedAbm);
+            }
+        }
+        return false;
+    }
 
 }
