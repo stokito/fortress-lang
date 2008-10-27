a1d525c9966893dd75429f53cb62b4fa9e31c19e
skilpat <skilpat@localhost>
2008-10-27 04:19:12 +0000

    [scortress] Now it's really been added.

---
+++ ProjectFortress/src/com/sun/fortress/scala/scortress/constraints/constraints.scala
@@ -0,0 +1,36 @@
+/*******************************************************************************
+    Copyright 2008 Sun Microsystems, Inc.,
+    4150 Network Circle, Santa Clara, California 95054, U.S.A.
+    All rights reserved.
+
+    U.S. Government Rights - Commercial software.
+    Government users are subject to the Sun Microsystems, Inc. standard
+    license agreement and applicable provisions of the FAR and its supplements.
+
+    Use is subject to license terms.
+
+    This distribution may include materials developed by third parties.
+
+    Sun, Sun Microsystems, the Sun logo and Java are trademarks or registered
+    trademarks of Sun Microsystems, Inc. in the U.S. and other countries.
+ ******************************************************************************/
+
+package com.sun.fortress.scala.scortress.constraints
+
+import com.sun.fortress.scala.scortress.types._
+
+/** A static constraint for the validity of the program. */
+abstract sealed class Constraint
+
+/** A constraint of the form {@code C1 AND C2}. */
+case class CnConj(c1:Constraint, c2:Constraint) extends Constraint
+
+/** A constraint of the form {@code C1 OR C2}. */
+case class CnDisj(c1:Constraint, c2:Constraint) extends Constraint
+
+/** A constraint of the form {@code T1 == T2}. */
+case class CnEquation(t1:Type, t2:Type) extends Constraint
+
+/** A constraint of the form {@code T1 <: T2}. */
+case class CnSubtype(t1:Type, t2:Type) extends Constraint
+
