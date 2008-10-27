508c6aa55012a3887b2ddad1e3d09e0afa4b9634
skilpat <skilpat@localhost>
2008-10-27 04:55:13 +0000

    [scortress] expr -> term

--- ProjectFortress/src/com/sun/fortress/scala/scortress/terms/terms.scala
+++ ProjectFortress/src/com/sun/fortress/scala/scortress/terms/terms.scala
@@ -15,54 +15,54 @@
     trademarks of Sun Microsystems, Inc. in the U.S. and other countries.
  ******************************************************************************/
 
-package com.sun.fortress.scala.scortress.exprs
+package com.sun.fortress.scala.scortress.terms
 
 import com.sun.fortress.scala.scortress.types._
 
 /** An expression in our language. */
-abstract sealed class Expr
+abstract sealed class Term
 
 /** A variable reference. */
-case class ExVariable(name:String) extends Expr
+case class TmVariable(name:String) extends Term
 
 /** A tuple of expressions of size two or greater. */
-case class ExTuple(elts:List[Expr]) extends Expr
+case class TmTuple(elts:List[Term]) extends Term
 
 /** An instantiation of an object. */
-case class ExObjInst(obj:String,
+case class TmObjInst(obj:String,
                      sargs:Option[List[Type]],
-                     args:List[Expr]) extends Expr
+                     args:List[Term]) extends Term
 
 /** An anonymous function expression. */
-case class ExAnonFn(params:List[Pair[String, Option[Type]]],
+case class TmAnonFn(params:List[Pair[String, Option[Type]]],
                     range:Option[Type],
-                    body:Expr) extends Expr
+                    body:Term) extends Term
 
 /** A dotted method invocation. */
-case class ExInvok(obj:Expr,
+case class TmInvok(obj:Term,
                    name:String,
                    sargs:Option[List[Type]],
-                   args:List[Expr]) extends Expr
+                   args:List[Term]) extends Term
 
 /**
  * A juxtaposition of two other expressions as created by the parser. This could
- * be rewritten into either {@code Ex_OpApp} or {@code Ex_FnApp}, depending on
+ * be rewritten into either {@code Tm_OpApp} or {@code Tm_FnApp}, depending on
  * the type of its subexpressions.
  */
-case class ExJuxt(left:Expr, right:Expr) extends Expr
+case class TmJuxt(left:Term, right:Term) extends Term
 
 /**
  * An application of the juxtaposition operator to two argument expressions.
- * After parsing, there is only a {@code ExJuxt} node. The type checker will
+ * After parsing, there is only a {@code TmJuxt} node. The type checker will
  * create this rewritten node if it is judged to be a juxtaposition and not a
  * function application.
  */
-case class Ex_OpApp(left:Expr, right:Expr) extends Expr
+case class Tm_OpApp(left:Term, right:Term) extends Term
 
 /**
  * An application of some arrow-typed term to an operand. After
- * parsing, there is only a {@code ExJuxt} node. The type checker will create
+ * parsing, there is only a {@code TmJuxt} node. The type checker will create
  * this rewritten node if it is judged to be a juxtaposition and not a function
  * application.
  */
-case class Ex_FnApp(app:Expr, opr:Expr) extends Expr
+case class Tm_FnApp(app:Term, opr:Term) extends Term
