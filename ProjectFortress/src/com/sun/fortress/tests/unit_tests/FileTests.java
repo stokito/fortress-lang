417572eb4232026a1abdf14f694c0bd1af2f4b2c
dr2chase <dr2chase@localhost>
2008-12-11 21:51:02 +0000

    [unit tests] added recognition of .sh file type for testing -- shell scripts return 0/1 for success/failure

--- ProjectFortress/src/com/sun/fortress/tests/unit_tests/FileTests.java
+++ ProjectFortress/src/com/sun/fortress/tests/unit_tests/FileTests.java
@@ -17,8 +17,12 @@
 
 package com.sun.fortress.tests.unit_tests;
 import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -38,6 +42,7 @@ import com.sun.fortress.nodes.Component;
 import com.sun.fortress.nodes_util.NodeFactory;
 import com.sun.fortress.repository.ProjectProperties;
 import com.sun.fortress.repository.GraphRepository;
+import com.sun.fortress.useful.StreamForwarder;
 import com.sun.fortress.useful.Useful;
 import com.sun.fortress.useful.WireTappedPrintStream;
 
@@ -45,7 +50,7 @@ import edu.rice.cs.plt.iter.IterUtil;
 
 public class FileTests {
 
-    public static class FSSTest extends TestCase {
+    public static class BaseTest extends TestCase {
         String f;
         String dir;
         String name;
@@ -55,12 +60,12 @@ public class FileTests {
          * If true, only print test output for unexpected results.
          */
         boolean unexpectedOnly;
-
+        boolean expectFailure;
 
         boolean printSuccess;
         boolean printFailure;
 
-        public FSSTest(String path, String d, String s, boolean unexpected_only, boolean expect_failure) {
+        public BaseTest(String path, String d, String s, boolean unexpected_only, boolean expect_failure) {
             super("testFile");
             this.f = d + "/" + s;
             this.dir = d;
@@ -69,10 +74,77 @@ public class FileTests {
             this.unexpectedOnly = unexpected_only;
             this.printSuccess = !unexpected_only || expect_failure;
             this.printFailure = !unexpected_only || !expect_failure;
+            this.expectFailure = expect_failure;
         }
-
+        
         public String getName() {
             return f;
+        }
+        
+    }
+    
+    public static class ShellTest extends BaseTest {
+        
+        public ShellTest(String path, String d, String s, boolean unexpected_only, boolean expect_failure) {
+            super(path, d, s, unexpected_only, s.startsWith("XXX") ? !expect_failure : expect_failure);
+        }
+
+        public void testFile() throws Throwable {
+            String scriptName = f + ".sh";
+            Runtime runtime = Runtime.getRuntime();
+            System.out.print("  ") ; System.out.print(f); System.out.print(" "); System.out.flush();
+            
+            Process p = runtime.exec(scriptName);
+            InputStream err = p.getErrorStream();
+            InputStream out = p.getInputStream();
+            OutputStream in = p.getOutputStream();
+            in.close();
+            
+            ByteArrayOutputStream cached_err = new ByteArrayOutputStream();
+            ByteArrayOutputStream cached_out = new ByteArrayOutputStream();
+            
+            StreamForwarder f_out = new StreamForwarder(out, cached_out, true);
+            StreamForwarder f_err = new StreamForwarder(err, cached_err, true);
+            
+                f_out.join();
+                f_err.join();
+                int exitValue = p.waitFor();
+                String s_out = cached_out.toString();
+                String s_err = cached_err.toString();
+                
+                boolean fail_exit = exitValue != 0;
+                // We've decided that exit codes are definitive.
+                boolean fail_out =  false && s_out.contains("FAIL");
+                boolean fail_err =  false && s_err.contains("FAIL");
+                boolean failed = fail_exit || fail_out || fail_err;
+                String fail_cause = !failed ? "" : (
+                        (fail_exit ? "Exit code != 0" : "") +
+                        (fail_out ? " Stdout contained FAIL" : "") +
+                        (fail_err ? " Stderr contained FAIL" : "")
+                ).trim();
+                
+                if (failed && printFailure || !failed && printSuccess) {
+                    System.out.print(s_out);
+                    System.out.print(s_err);
+                }
+                
+                if (expectFailure != failed) {
+                    System.out.println(failed ? "UNEXPECTED failure ("+ fail_cause+")" : "Did not see expected failure");
+                    fail();
+                } else {
+                    System.out.println(failed ? "Saw expected failure ("+ fail_cause+")" : "Passed");
+                }
+
+           
+            
+
+        }
+    }
+    
+    public static class FSSTest extends BaseTest {
+        
+        public FSSTest(String path, String d, String s, boolean unexpected_only, boolean expect_failure) {
+            super(path, d, s, unexpected_only, expect_failure);
         }
 
         public void testFile() throws Throwable {
@@ -90,7 +162,6 @@ public class FileTests {
             System.setOut(wt_out);
 
             String s = f.replaceFirst(".*/", "");
-            String tmpFile = "/tmp/" + s + ".ast";
             String fssFile = f + ".fss";
 
             BufferedReader in = Useful.utf8BufferedFileReader(fssFile);
@@ -200,6 +271,7 @@ public class FileTests {
 
     }
     public static void main(String[] args) throws IOException {
+        junit.textui.TestRunner.run(FileTests.suite("shelltests", true, false));
         junit.textui.TestRunner.run(FileTests.suite("tests", true, false));
         junit.textui.TestRunner.run(FileTests.suite("not_passing_yet", false, true));
     }
@@ -213,6 +285,14 @@ public class FileTests {
         };
         String dirname = ProjectProperties.backslashToSlash(dir_name);
         File dir = new File(dirname);
+        if (!dir.exists()) {
+            System.err.println(dirname + " does not exist");
+            throw new FileNotFoundException(dirname);
+        }
+        if (!dir.isDirectory()) {
+            System.err.println(dirname + " exists but is not a directory");
+            throw new IllegalArgumentException(dirname);
+        }
         String[] files = dir.list();
         System.err.println(dir);
         Iterable<String> shuffled = IterUtil.shuffle(Arrays.asList(files));
@@ -224,6 +304,10 @@ public class FileTests {
                       int l = s.lastIndexOf(".fss");
                       String testname = s.substring(0, l);
                       suite.addTest(new FSSTest(dir.getCanonicalPath(), dirname, testname, failsOnly, expect_failure));
+                  } else if (s.endsWith(".sh")) {
+                      int l = s.lastIndexOf(".sh");
+                      String testname = s.substring(0, l);
+                      suite.addTest(new ShellTest(dir.getCanonicalPath(), dirname, testname, failsOnly, expect_failure));
                   } else {
                       System.out.println("Not compiling file " + s);
                   }
