a94db3eb348828df8d2632e711f8e8ce1f40af92
jmaessen <jmaessen@localhost>
2008-12-12 22:18:46 +0000

    Frobbed to make a bit more location-independent.

--- ProjectFortress/tests/concurrentPrinting.sh
+++ ProjectFortress/tests/concurrentPrinting.sh
@@ -1,3 +1,5 @@
-#! /bin/sh
-./bin/fortress run ProjectFortress/tests/printing/ConcurrentPrinting.fss > ProjectFortress/tests/printing/test_output.txt
-cmp ProjectFortress/tests/printing/test_output.txt ProjectFortress/tests/printing/test_oracle.txt
+#!/bin/sh
+cd `dirname $0`
+rm printing/test_output.txt
+../../bin/fortress run printing/ConcurrentPrinting.fss > printing/test_output.txt
+cmp printing/test_output.txt printing/test_oracle.txt
