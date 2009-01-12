78689aafad72bcd591db995d42e5ffd79b6ebf10
dr2chase <dr2chase@localhost>
2009-01-12 16:52:24 +0000

    [respository/foreign] removed use of reflection, uses asm instead

---
+++ ProjectFortress/src/com/sun/fortress/compiler/NamingCzar.java
@@ -0,0 +1,13 @@
+/*
+ * Created on Jan 8, 2009
+ *
+ */
+package com.sun.fortress.compiler;
+
+import com.sun.fortress.nodes.APIName;
+
+public class NamingCzar {    
+    static public String cachedAstNameForApi(APIName a) {
+        return "";
+    }
+}
