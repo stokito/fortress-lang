fd00b4351ea7363c7669fe2512b89fce11173c93
sukyoungryu <sukyoungryu@localhost>
2009-02-19 17:49:18 +0000

    [parser] Revised the literals and some expressions grammar.

--- ProjectFortress/src/com/sun/fortress/compiler/typechecker/constraints/ConstraintUtil.java
+++ ProjectFortress/src/com/sun/fortress/compiler/typechecker/constraints/ConstraintUtil.java
@@ -1,3 +1,20 @@
+/*******************************************************************************
+    Copyright 2009 Sun Microsystems, Inc.,
+    4150 Network Circle, Santa Clara, California 95054, U.S.A.
+    All rights reserved.
+
+    U.S. Government Rights - Commercial software.
+    Government users are subject to the Sun Microsystems, Inc. standard
+    license agreement and applicable provisions of the FAR and its supplements.
+
+    Use is subject to license terms.
+
+    This distribution may include materials developed by third parties.
+
+    Sun, Sun Microsystems, the Sun logo and Java are trademarks or registered
+    trademarks of Sun Microsystems, Inc. in the U.S. and other countries.
+ ******************************************************************************/
+
 package com.sun.fortress.compiler.typechecker.constraints;
 
 import com.sun.fortress.compiler.typechecker.constraints.JavaConstraintUtil;
@@ -7,24 +24,24 @@ import com.sun.fortress.compiler.typechecker.*;
 
 /**
  * Temporary Factory that can produce both Java and Scala
- * constraint formulas 
+ * constraint formulas
  */
-public class ConstraintUtil {	
-	
+public class ConstraintUtil {
+
 	private static boolean java = true;
-	
+
 	public static void useJavaFormulas(){
 		java = true;
 	}
-	
+
 	public static void useScalaFormulas(){
 		java = false;
 	}
-	
+
 	public static void setJava(Boolean _java){
 		java = _java;
 	}
-	
+
 	public static ConstraintFormula trueFormula() {
 		if(java)
 			return JavaConstraintUtil.trueFormula();
@@ -37,21 +54,21 @@ public class ConstraintUtil {
 		else
 			return ScalaConstraintUtil.falseFormula();
 	}
-	
+
 	public static ConstraintFormula upperBound(_InferenceVarType ivar, Type type, SubtypeHistory h) {
 		if(java)
 			return JavaConstraintUtil.upperBound(ivar,type,h);
 		else
 			return ScalaConstraintUtil.upperBound(ivar,type,h);
 	}
-	
+
 	public static ConstraintFormula lowerBound(_InferenceVarType ivar, Type type, SubtypeHistory h) {
 		if(java)
 			return JavaConstraintUtil.lowerBound(ivar,type,h);
 		else
 			return ScalaConstraintUtil.lowerBound(ivar,type,h);
 	}
-	
+
 	public static ConstraintFormula bigAnd(Iterable<? extends ConstraintFormula> constraints,
 			SubtypeHistory hist) {
 		ConstraintFormula result = trueFormula();
@@ -60,12 +77,12 @@ public class ConstraintUtil {
 		}
 		return result;
 	}
-	
+
 	public static ConstraintFormula fromBoolean(Boolean bool){
 		if(java)
 			return JavaConstraintUtil.fromBoolean(bool);
 		else
 			return ScalaConstraintUtil.fromBoolean(bool);
 	}
-	
+
 }
