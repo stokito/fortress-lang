bcb5698c57ff2e5883689cfa29508015a01deb11
jmaessen <jmaessen@localhost>
2009-04-13 20:10:40 +0000

    [compiler] Boxed integer arithmetic via native calls.

---
+++ ProjectFortress/src/com/sun/fortress/nativeHelpers/simpleIntArith.java
@@ -0,0 +1,30 @@
+/*******************************************************************************
+    Copyright 2009 Sun Microsystems, Inc.,
+    4150 Network Circle, Santa Clara, California 95054, U.S.A.
+    All rights reserved.
+
+    U.S. Government Rights - Commercial software.
+    Government users are subject to the Sun Microsystems, Inc. standard
+    license agreement and applicable provisions of the FAR and its supplements.
+
+    Use is subject to license terms.
+
+    This distribution may include materials developed by third parties.
+
+    Sun, Sun Microsystems, the Sun logo and Java are trademarks or registered
+    trademarks of Sun Microsystems, Inc. in the U.S. and other countries.
+ ******************************************************************************/
+
+package com.sun.fortress.nativeHelpers;
+
+public class simpleIntArith {
+
+    public static String intToString(int x) {
+        return Integer.toString(x);
+    }
+
+    public static int intAdd(int a, int b) { return a+b; }
+    public static int intSub(int a, int b) { return a-b; }
+    public static int intMul(int a, int b) { return a*b; }
+    public static int intNeg(int a) { return -a; }
+}
