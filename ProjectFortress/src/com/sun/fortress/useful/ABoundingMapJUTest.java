d23364c762493bffdd5781e66ef5cbce59a8c467
sukyoungryu <sukyoungryu@localhost>
2009-04-13 15:50:38 +0000

    [static checker] Implemented declared exclusion relationships between non-generic types.  Added tests for the exclusion checking.

--- ProjectFortress/src/com/sun/fortress/useful/ABoundingMapJUTest.java
+++ ProjectFortress/src/com/sun/fortress/useful/ABoundingMapJUTest.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
-    Copyright 2008 Sun Microsystems, Inc.,
+    Copyright 2009 Sun Microsystems, Inc.,
     4150 Network Circle, Santa Clara, California 95054, U.S.A.
     All rights reserved.
 
@@ -35,7 +35,7 @@ public class ABoundingMapJUTest extends TestCaseWrapper {
     public void testABoundingMapMapOfTULatticeOpsOfU() {
         // TODO Auto-generated method stub
         System.err.println("before a success");
-        
+
     }
 
     /*
