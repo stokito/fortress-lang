171d94d249221ee996e0610fe461dfd15a701447
sukyoungryu <sukyoungryu@localhost>
2009-07-13 15:37:17 +0000

    [static checker] Refactoring IndexBuilder. (work in progress)

--- ProjectFortress/src/com/sun/fortress/compiler/StaticPhaseResult.java
+++ ProjectFortress/src/com/sun/fortress/compiler/StaticPhaseResult.java
@@ -22,26 +22,29 @@ import com.sun.fortress.compiler.typechecker.TypeCheckerResult;
 import com.sun.fortress.exceptions.StaticError;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class StaticPhaseResult {
-    
-    private final Iterable<? extends StaticError> _errors;
-    
+
+    private Iterable<? extends StaticError> _errors;
+
     public StaticPhaseResult() {
         this(IterUtil.<StaticError>empty());
     }
-    
+
     public StaticPhaseResult(Iterable<? extends StaticError> errors) {
         _errors = errors;
     }
-    
+
     public StaticPhaseResult(StaticPhaseResult r1, StaticPhaseResult r2) {
         _errors = IterUtil.compose(r1._errors, r2._errors);
     }
-    
+
     public boolean isSuccessful() { return IterUtil.isEmpty(_errors); }
-    
+
     public Iterable<? extends StaticError> errors() { return _errors; }
+
+    public void setErrors(List<StaticError> errors) { _errors = errors; }
 
     protected static Iterable<? extends StaticError> collectErrors(Iterable<? extends TypeCheckerResult> results) {
         Iterable<? extends StaticError> allErrors = new ArrayList<StaticError>();
