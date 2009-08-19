326914e4d6f8a42c47e48ec3e73f50d45ba6aaa6
jrhil47 <jrhil47@localhost>
2009-08-19 17:05:32 +0000

    [Typechecker] Fixed a bug that was causing us to drop arguments.

--- ProjectFortress/src/com/sun/fortress/scala_src/typechecker/impls/Functionals.scala
+++ ProjectFortress/src/com/sun/fortress/scala_src/typechecker/impls/Functionals.scala
@@ -355,11 +355,10 @@ trait Functionals { self: STypeChecker with Common =>
       // We need to make sure there are the right number of args for the domain.
       // Now that coercions are in place, we can simply check if the new arg
       // type is a subtype of the domain type.
-      val argType = getArgType(newArgs)
-      if (isSubtype(argType, arrow.getDomain))
+      if (newArgs.size == args.size)
         Left((arrow, Nil, newArgs.map(_.left.get)))
       else
-        Right(errorFactory.makeNotApplicableError(originalArrow, newArgs))
+        Right(errorFactory.makeNotApplicableError(originalArrow, args))
     }
   }
 
