6b7c3ed4cb5791f2b0258294e74f1e70dff02310
dr2chase <dr2chase@localhost>
2009-12-11 17:30:27 +0000

    Some work done on the classloader-renamer to make it cope with non-numbered static parameter names

---
+++ ProjectFortress/src/com/sun/fortress/runtimeSystem/InstantiationMapJUTest.java
@@ -0,0 +1,82 @@
+/*******************************************************************************
+    Copyright 2009 Sun Microsystems, Inc.,
+    4150 Network Circle, Santa Clara, California 95054, U.S.A.
+    All rights reserved.
+
+    U.S. Government Rights - Commercial software.
+    Government users are subject to the Sun Microsystems, Inc. standard
+    license agreement and applicable provisions of the FAR and its supplements.
+
+    Use is subject to license terms.
+
+    This distribution may include materials developed by third parties.
+
+    Sun, Sun Microsystems, the Sun logo and Java are trademarks or registered
+    trademarks of Sun Microsystems, Inc. in the U.S. and other countries.
+******************************************************************************/
+package com.sun.fortress.runtimeSystem;
+
+import java.util.HashMap;
+
+import junit.framework.TestCase;
+
+public class InstantiationMapJUTest extends TestCase {
+
+    InstantiationMap map = newInstantiationMap(); 
+    static InstantiationMap newInstantiationMap() {
+        HashMap<String, String> map = new HashMap<String, String>();
+        map.put("A", "Xant");
+        map.put("B", "Xbat");
+        map.put("C", "Xcat");
+        map.put("D", "Xdog");
+        map.put("E", "Xemu");
+        map.put("F", "Xfox");
+        return new InstantiationMap(map);
+    }
+    
+    public void testMaybeVarInOxfords() {
+        StringBuffer b = new StringBuffer();
+        int i = map.maybeVarInOxfords("A"+Naming.RIGHT_OXFORD, 0, b);
+  
+        assertEquals(2, i);
+        assertEquals("Xant"+Naming.RIGHT_OXFORD, b.toString());
+ 
+        b = new StringBuffer();
+        i = map.maybeVarInOxfords("BB"+Naming.RIGHT_OXFORD, 0, b);
+        assertEquals(3, i);
+        assertEquals("BB"+Naming.RIGHT_OXFORD, b.toString());
+
+        b = new StringBuffer();
+        i = map.maybeVarInOxfords("C;D"+Naming.RIGHT_OXFORD, 0, b);
+        assertEquals(4, i);
+        assertEquals("Xcat;Xdog"+Naming.RIGHT_OXFORD, b.toString());
+        
+        b = new StringBuffer();
+        i = map.maybeVarInOxfords("C=C;D=D"+Naming.RIGHT_OXFORD, 0, b);
+        assertEquals(8, i);
+        assertEquals("C=Xcat;D=Xdog"+Naming.RIGHT_OXFORD, b.toString());
+    }
+
+    public void testMaybeVarInLSemi() {
+        // fail("Not yet implemented"); // TODO
+    }
+
+    public void testMaybeBareVar() {
+        StringBuffer b = new StringBuffer();
+        int i = map.maybeBareVar("A", 0, b, false);
+        assertEquals(1, i);
+        assertEquals("ant", b.toString());
+ 
+        b = new StringBuffer();
+        i = map.maybeBareVar("B;", 0, b, false);
+        assertEquals(1, i);
+        assertEquals("bat", b.toString());
+
+        b = new StringBuffer();
+        i = map.maybeBareVar("CC;", 0, b, false);
+        assertEquals(2, i);
+        assertEquals("CC", b.toString());
+
+    }
+
+}
