0b50205d5e55f6f1a81b264772e7d6c5891f90c2
jmaessen <jmaessen@localhost>
2010-02-08 16:04:06 +0000

    [Copyright]

--- ProjectFortress/src/com/sun/fortress/interpreter/glue/prim/FlatString.java
+++ ProjectFortress/src/com/sun/fortress/interpreter/glue/prim/FlatString.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- Copyright 2009 Sun Microsystems, Inc.,
+ Copyright 2010 Sun Microsystems, Inc.,
  4150 Network Circle, Santa Clara, California 95054, U.S.A.
  All rights reserved.
 
@@ -196,7 +196,7 @@ public class FlatString extends NativeConstructor {
             return Pattern.matches(match, self);
         }
     }
-    
+
     public static final class javaRegExpSplit extends ssI2s {
         protected String f(java.lang.String self, java.lang.String match, int index) {
             Pattern p = Pattern.compile(match);
