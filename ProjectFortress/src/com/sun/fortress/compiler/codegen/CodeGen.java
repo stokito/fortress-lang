4fd7e8e609f8aaae4adb61d61194648e923ac528
chf <chf@localhost>
2010-03-24 18:07:00 +0000

    undo unintentional change

--- ProjectFortress/src/com/sun/fortress/compiler/codegen/CodeGen.java
+++ ProjectFortress/src/com/sun/fortress/compiler/codegen/CodeGen.java
@@ -24,7 +24,6 @@ import java.math.BigInteger;
 import java.util.*;
 import java.util.jar.JarOutputStream;
 
-import org.objectweb.asm.Opcodes;
 import org.objectweb.asm.*;
 import org.objectweb.asm.util.*;
 
