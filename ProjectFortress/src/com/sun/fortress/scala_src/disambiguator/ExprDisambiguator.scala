d53aab64dca0477e965413f8d05b587dc8b56e1b
sukyoungryu <sukyoungryu@localhost>
2010-04-02 05:53:46 +0000

    [demos] Fixed a broken demo due to library changes.

--- ProjectFortress/src/com/sun/fortress/scala_src/disambiguator/ExprDisambiguator.scala
+++ ProjectFortress/src/com/sun/fortress/scala_src/disambiguator/ExprDisambiguator.scala
@@ -1,18 +1,18 @@
 /*******************************************************************************
- Copyright 2010 Sun Microsystems, Inc.,
- 4150 Network Circle, Santa Clara, California 95054, U.S.A.
- All rights reserved.
+    Copyright 2010 Sun Microsystems, Inc.,
+    4150 Network Circle, Santa Clara, California 95054, U.S.A.
+    All rights reserved.
 
- U.S. Government Rights - Commercial software.
- Government users are subject to the Sun Microsystems, Inc. standard
- license agreement and applicable provisions of the FAR and its supplements.
+    U.S. Government Rights - Commercial software.
+    Government users are subject to the Sun Microsystems, Inc. standard
+    license agreement and applicable provisions of the FAR and its supplements.
 
- Use is subject to license terms.
+    Use is subject to license terms.
 
- This distribution may include materials developed by third parties.
+    This distribution may include materials developed by third parties.
 
- Sun, Sun Microsystems, the Sun logo and Java are trademarks or registered
- trademarks of Sun Microsystems, Inc. in the U.S. and other countries.
+    Sun, Sun Microsystems, the Sun logo and Java are trademarks or registered
+    trademarks of Sun Microsystems, Inc. in the U.S. and other countries.
  ******************************************************************************/
 
 package com.sun.fortress.scala_src.disambiguator;
@@ -693,7 +693,17 @@ class ExprDisambiguator(compilation_unit: CompilationUnit,
         !uninitializedNames.contains(v)) {
       if (!env.explicitVariableNames(v).isEmpty ||
           !env.explicitFunctionNames(v).isEmpty)
+          {
+              /* for debugging
+              for (id <- toSet(env.explicitVariableNames(v)) ) {
+                  System.err.println("explicitVariableNames " + id.toStringVerbose)
+              }
+              for (id <- toSet(env.explicitFunctionNames(v)) ) {
+                  System.err.println("explicitFunctionNames " + id.toStringVerbose)
+              }
+              */
         error("Variable " + v + " is already declared.", v)
+          }
     }
   }
 
