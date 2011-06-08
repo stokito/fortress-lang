92379aa0bdcac1f23ad008918b4d284003e8a951
karl.naden <karl.naden@localhost>
2011-06-08 18:33:00 -0400

    arrowRTTI framework

--- ProjectFortress/src/com/sun/fortress/compiler/codegen/CodeGen.java
+++ ProjectFortress/src/com/sun/fortress/compiler/codegen/CodeGen.java
@@ -3456,6 +3456,7 @@ public class CodeGen extends NodeAbstractVisitor_void implements Opcodes {
         areturnEpilogue();
         
         emitRttiField(cnb);
+        /* end RTTI stuff */
         
         lexEnv = savedLexEnv;
 
