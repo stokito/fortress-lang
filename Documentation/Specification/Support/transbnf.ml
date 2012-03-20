4f450fc91b6f4502cd6152799abbfb6bd40de09e
tristan <tristan@localhost>
2012-03-19 23:18:44 -0400

    More BNF parsing

--- Documentation/Specification/Support/transbnf.ml
+++ Documentation/Specification/Support/transbnf.ml
@@ -1,77 +1,165 @@
 
 open String
 
-type choices = string list
+type choice = string list
+type choices = choice list
 type entry = string * choices
 type entries = entry list
 type section = string * entries
 type grammar = section list
 
 type kind = 
-  | Section of string
-  | Entry of string
-  | Choice of string
+  | Section of string 
+  | Entry of string 
+  | Choice of string list
 
-let scan_section line =
-  if length line < 5 then None else 
-    if sub line 0 5 = "$$$$$" then 
-      Some (sub line 5 (length line - 5))
-    else None
+let rec print_line line = 
+  match line with
+    | [] -> ()
+    | x :: tl -> print_string x; print_string "."; print_line tl
 
-let scan_entry line =
-  let l = length line in
-  if l < 3 then None else
-    if sub line (l-3) 3 = "::=" then Some (sub line 0 (l - 3))
-    else None
+let print_kind k = 
+  match k with
+    | Section s -> Printf.printf "Section %s\n" s
+    | Entry s -> Printf.printf "Entry %s\n" s
+    | Choice l -> print_line l
 
-let scan_choice line line_counter = 
-  let l = length line in
-  if contains line '|' then
-    let index = index line '|' in
-    let _ =
-      if contains_from line (index + 1) '|' then 
-	let error_msg = "Warning: line " ^ string_of_int(line_counter) ^ " (" ^ line  ^ ")" ^ " has several choices.\n" in
-	print_string error_msg 
-    in
-      Some (sub line (index + 1) (l - index - 1))
-  else None
+let rec split_aux line n word words = 
+  if n = length line then 
+    if word = [] 
+    then words 
+    else word :: words
+  else 
+    let c = get line n in
+    if c = ' ' && word != [] 
+    then split_aux line (n + 1) [] (word :: words)
+    else 
+      if c = ' ' 
+      then split_aux line (n + 1) [] words
+      else split_aux line (n + 1) (Char.escaped c :: word) words
+
+let split line = 
+  let l = split_aux line 0 [] [] in
+  let l = List.map List.rev l in
+  let l = List.map (concat "") l in
+  List.rev l
 
 let analyze line line_counter = 
-  match scan_choice line line_counter with
-    | Some s -> Choice s
-    | None -> 
-      begin
-	match scan_entry line with
-	  | Some s -> Entry s
-	  | None -> 
-	    begin
-	      match scan_section line with
-		| Some s -> Section s
-		| None -> 
-		  let error_msg = "Failed to recognize line " ^ (string_of_int line_counter) ^ " ( " ^ line ^  " ). " in
-		    failwith error_msg
-	    end
-      end
+  let l = split line in
+  let b1 = List.mem "$$$$$" l
+  and b2 = List.mem "::=" l
+  and b3 = List.mem "|" l in
+  match b1, b2, b3 with
+    | true, false, false -> 
+      if List.nth l 0 = "$$$$$" then Section (concat " " (List.tl l)) 
+      else
+	let error_msg = "Error: pattern $$$$$ does not start line " ^ (string_of_int line_counter) ^ " |" ^ line ^ "| " in
+	failwith error_msg
+    | false, true, false -> 
+      if List.length l != 2 
+      then 
+	let error_msg = "Error: line " ^ (string_of_int line_counter) ^ " has a pattern ::= but more than two elements" ^ " |" ^ line ^ "| "
+	in failwith error_msg
+      else
+	if List.nth l 1 <> "::="
+	then
+	  let error_msg = "Error: pattern ::= does not end line " ^ (string_of_int line_counter) ^ " |" ^ line ^ "| " in
+	  print_string (List.nth l 1);
+	  failwith error_msg
+	else Entry (List.nth l 0)
+    | false, false, true ->
+      if (List.nth l 0) <> "|"
+      then 
+	let error_msg = "Error: pattern | does not start line " ^ (string_of_int line_counter) ^ " |" ^ line ^ "| " ^ (List.nth l 0 ) ^ " instead"  in
+	failwith error_msg
+      else Choice (List.tl l)
+    | false, false, false -> 
+      let error_msg = "Error: cannot recognize line " ^ (string_of_int line_counter) ^ " |" ^ line ^ "| "  in 
+      failwith error_msg
+    | _ -> 
+      let error_msg = "Error: ambiguous line " ^ (string_of_int line_counter) ^ " |" ^ line ^ "| " in
+      failwith error_msg
 	
-let parse line line_counter =
-  if length line <= 0 then () else 
-    let s = 
-      match analyze line line_counter with
-	| Section s -> Printf.sprintf "Section %s\n" s
-	| Entry s -> Printf.sprintf "Entry %s\n" s
-	| Choice s -> Printf.sprintf "Choice %s\n" s
-    in () (* print_string s; flush stdout *)
+let rec parse_choices l: choices * kind list =
+  match l with
+    | Choice rhs :: tl -> 
+      let (choices,l) = parse_choices tl in
+      (rhs :: choices, l)
+    | _ -> ([],l) 
 
+let rec parse_entry l: entry option * kind list =
+  match l with
+    | Entry e :: tl -> 
+      let (choices,rem) = parse_choices tl in
+      (Some (e,choices), rem) 
+    | _ -> (None,l)
+
+let rec parse_entries l: entries * kind list = 
+  match parse_entry l with
+    | Some (e,choices), l -> 
+      let (entries,rem) = parse_entries l in
+      (((e,choices) :: entries), rem)
+    | _ -> ([],l)
+
+let parse_section l: section option * kind list = 
+  match l with
+    | Section s :: tl -> 
+      let (res,rem) = parse_entries tl in
+      (Some (s,res),rem)
+    | _ -> (None,l)
+
+let emptyg : grammar = []
+
+let rec parse_sections l: grammar * kind list = 
+  match parse_section l with
+    | Some section, tl ->
+      let (res,rem) = parse_sections tl in
+      (section :: res,rem)
+    | _ -> (emptyg,l)
+
+let rec parse_grammar l = 
+  match parse_sections l with
+    | g,[] -> g
+    | g,rem -> failwith "BNF parsing error"
+
+let pp_choice choice = " & " ^ " & " ^ (concat " " choice) ^ " \\\\"
+
+let pp_choices choices = List.map pp_choice choices
+
+let pp_entry (header,choices) = 
+  let h = Printf.sprintf "\\begin{tabular}{lll}" in
+  let s = header ^ " & " ^ " ::= " ^ " & " ^ " \\\\"  in
+  let e = Printf.sprintf "\\end{tabular}" in
+  h :: s :: ((pp_choices choices) @ [e]) 
+
+let pp_entries entries = List.flatten (List.map pp_entry entries)
+
+let pp_section (title,entries): string list = 
+  let sec = Printf.sprintf "\\section{%s}" title in
+  sec :: (pp_entries entries)
+
+let pp_sections sections: string list = 
+  List.flatten (List.map pp_section sections) 
+
+let pp_grammar = pp_sections
 
 let _ = 
   let ic = open_in "../Data/bnf.txt" in
   let oc = open_out "../Data/bnf.tex" in
   let line_counter = ref 1 in
-  try 
-    while true do
-      let s = input_line ic in 
-      let _ = parse s !line_counter in
-      (* output_string oc s; *)
-      incr line_counter
-    done
-  with End_of_file -> ()
+  let in_file = ref [] in
+  let _ = 
+    try 
+      while true do
+	let s = input_line ic in 
+	if length s > 0 then in_file := (analyze s !line_counter) :: !in_file;
+	      (* output_string oc s; *)
+	incr line_counter
+      done
+    with End_of_file -> () in
+  let read = List.rev !in_file in
+  let g = parse_grammar read in
+  let g = pp_grammar g in
+  List.iter (fun x -> Printf.printf "%s\n" x) g
+ 
+    
