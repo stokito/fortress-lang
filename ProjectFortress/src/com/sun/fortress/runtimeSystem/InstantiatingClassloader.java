e83530318ce3211a40bfc4e3f0ce1bdee9f5db35
David Chase <david.r.chase@oracle.com>
2012-05-29 14:28:08 -0400

    Turned off som dbugging flags

--- ProjectFortress/src/com/sun/fortress/runtimeSystem/InstantiatingClassloader.java
+++ ProjectFortress/src/com/sun/fortress/runtimeSystem/InstantiatingClassloader.java
@@ -77,8 +77,8 @@ public class InstantiatingClassloader extends ClassLoader implements Opcodes {
     
     public static final int JVM_BYTECODE_VERSION = Opcodes.V1_6;
     // TODO make this depends on properties/env w/o dragging in all of the world.
-    private static final boolean LOG_LOAD_CHOICES = true;
-    static final boolean LOG_LOADS = true;
+    private static final boolean LOG_LOAD_CHOICES = false;
+    static final boolean LOG_LOADS = false;
     private static final boolean LOG_FUNCTION_EXPANSION = false;
     public final static String SAVE_EXPANDED_DIR = ProjectProperties.getDirectory("fortress.bytecodes.expanded.directory", null);
     public static JarOutputStream SAVE_EXPANDED_JAR = null;
